6bebb3e0612b5da2263de02a92029b0b10622646
majesticrock <joshua.althueser@web.de>
2022-12-15 11:56:43 +0100

    save

--- auto_commute.py
+++ auto_commute.py
@@ -1,4 +1,6 @@
 from dataclasses import dataclass
+from copy import deepcopy
+import numpy as np
 
 @dataclass
 class Momentum:
@@ -21,8 +23,6 @@ class Momentum:
 
     def additional_Q(self):
         self.add_Q = not self.add_Q
-
-    
 
 @dataclass
 class Operator:
@@ -54,7 +54,25 @@ class Operator:
             return "\\uparrow"
         return "\\downarrow"
 
-def print_duo(l, r):
+@dataclass
+class Term:
+    prefactor: int
+    coefficient: str
+    operators: np.ndarray
+
+@dataclass
+class Expression:
+    global_prefactor: int
+    terms: np.ndarray
+    def append(self, values):
+        np.append(self.terms, values)
+
+def sync_eps(momentum: Momentum, base=1):
+    if(momentum.add_Q):
+        return [-1*base, f"\\epsilon_{momentum.value}"]
+    return [1*base, f"\\epsilon_{momentum.value}"]
+
+def str_duo(l, r):
     if(l.daggered and not r.daggered):
         if(l.spin == r.spin):
             if(l.momentum.value == r.momentum.value and l.momentum.positve == r.momentum.positve):
@@ -79,9 +97,9 @@ def print_duo(l, r):
                             ret = f"\\eta_{{{r.momentum}}}"
                     else:
                         if(l.momentum.add_Q == r.momentum.add_Q):
-                            ret = f"- f_{{{l.momentum}}}"
+                            ret = f"(- f_{{{l.momentum}}})"
                         else:
-                            ret = f"- \\eta_{{{l.momentum}}}"
+                            ret = f"(- \\eta_{{{l.momentum}}})"
                 else:
                     if(not r.spin):
                         if(l.momentum.add_Q == r.momentum.add_Q):
@@ -90,33 +108,62 @@ def print_duo(l, r):
                             ret = f"\\eta_{{{l.momentum}}}"
                     else:
                         if(l.momentum.add_Q == r.momentum.add_Q):
-                            ret = f"- f_{{{r.momentum}}}"
+                            ret = f"(- f_{{{r.momentum}}})"
                         else:
-                            ret = f"- \\eta_{{{r.momentum}}}"
+                            ret = f"(- \\eta_{{{r.momentum}}})"
                     ret += "^\\dagger"
                 return ret
     
     return f"{l} {r}"
-            
 
-left  = Operator(Momentum("k", True, False), False, True)
-right = Operator(Momentum("k", True, False), False, False)
+def dagger_it(src):
+    src[0], src[1] = src[1], src[0]
+    src[0].daggered = not src[0].daggered
+    src[1].daggered = not src[1].daggered
 
-from copy import deepcopy
-commutator = f"\\left[H, {print_duo(left, right)}\\right] &= "
+N_U = [Operator(Momentum("k", True, False), True,  True), Operator(Momentum("k", True, False), True,  False)]
+N_D = [Operator(Momentum("k", True, False), False, True), Operator(Momentum("k", True, False), False, False)]
+
+SC   = [Operator(Momentum("k", False, False), False, False), Operator(Momentum("k", True, False), True, False)]
+SC_D = [Operator(Momentum("k", False, False), False, False), Operator(Momentum("k", True, False), True, False)]
+dagger_it(SC_D)
+
+CDW_U   = [Operator(Momentum("k", True, False), True,  True), Operator(Momentum("k", True, True), True,  False)]
+CDW_U_D = [Operator(Momentum("k", True, False), True,  True), Operator(Momentum("k", True, True), True,  False)]
+CDW_D   = [Operator(Momentum("k", True, False), False, True), Operator(Momentum("k", True, True), False, False)]
+CDW_D_D = [Operator(Momentum("k", True, False), False, True), Operator(Momentum("k", True, True), False, False)]
+dagger_it(CDW_U_D)
+dagger_it(CDW_D_D)
+
+ETA   = [Operator(Momentum("k", False, True), False, False), Operator(Momentum("k", True, False), True, False)]
+ETA_D = [Operator(Momentum("k", False, True), False, False), Operator(Momentum("k", True, False), True, False)]
+dagger_it(ETA_D)
+
+left  = ETA[0]
+right = ETA[1]
+
+comm_ex = Expression(1, np.array([], dtype=Term))
+
+commutator = f"\\left[H, {str_duo(left, right)}\\right] &= "
 swapped = False
 sign = ["+", "-"]
 if(left.daggered == right.daggered):
     if(left.daggered):
         sign = ["-", "+"]
         commutator += "- "
-    commutator += f"\\epsilon_{{{left.momentum}}} {print_duo(right, left)} {sign[1]} \\epsilon_{{{right.momentum}}} {print_duo(left, right)} \\\\"
+        comm_ex.global_prefactor = -1
+    
+    comm_ex.append([Term(*sync_eps(left.momentum), np.array([right, left])), Term(*sync_eps(left.momentum, -1), np.array([left, right]))])
+    
+    commutator += f"\\epsilon_{{{left.momentum}}} {str_duo(right, left)} {sign[1]} \\epsilon_{{{right.momentum}}} {str_duo(left, right)} \\\\"
     ##########
     buffer_l = deepcopy(left)
     buffer_l.additional_Q()
     buffer_r = deepcopy(right)
     buffer_r.additional_Q()
-    commutator += f"\n &{sign[0]} \\Delta_\\text{{CDW}} \\left( {print_duo(right, buffer_l)} - {print_duo(left, buffer_r)} \\right) \\\\"
+    comm_ex.append([Term(1, "\\Delta_\\text{{CDW}}", np.array([right, buffer_l])), Term(-1, "\\Delta_\\text{{CDW}}", np.array([right, buffer_l]))])
+
+    commutator += f"\n &{sign[0]} \\Delta_\\text{{CDW}} \\left( {str_duo(right, buffer_l)} - {str_duo(left, buffer_r)} \\right) \\\\"
     ##########
     # ensure that spin down is upfront
     if(left.spin):
@@ -131,7 +178,7 @@ if(left.daggered == right.daggered):
     buffer_l.make_sc()
     buffer_r = deepcopy(right)
     buffer_r.make_sc()
-    commutator += f"\\Delta_\\text{{SC}} \\left( {print_duo(buffer_l, right)} + {print_duo(buffer_r, left)}"
+    commutator += f"\\Delta_\\text{{SC}} \\left( {str_duo(buffer_l, right)} + {str_duo(buffer_r, left)}"
     if(left.momentum == buffer_r.momentum):
         commutator += " - 1"
     commutator += " \\right) \\\\"
@@ -145,7 +192,7 @@ if(left.daggered == right.daggered):
     commutator += "\\Delta_\\eta"
     if(left.daggered):
         commutator += "^*"
-    commutator += f" \\left( {print_duo(buffer_l, right)} + {print_duo(buffer_r, left)}"
+    commutator += f" \\left( {str_duo(buffer_l, right)} + {str_duo(buffer_r, left)}"
     if(left.momentum == buffer_r.momentum):
         commutator += " - 1"
     commutator += " \\right)"
@@ -159,13 +206,13 @@ else:
         if(left.momentum == right.momentum and left.spin == right.spin):
             commutator += "1 "
         commutator += "- "
-    commutator += f"\\epsilon_{{{left.momentum}}} {print_duo(left, right)} {sign[1]} \\epsilon_{{{right.momentum}}} {print_duo(left, right)} \\\\"
+    commutator += f"\\epsilon_{{{left.momentum}}} {str_duo(left, right)} {sign[1]} \\epsilon_{{{right.momentum}}} {str_duo(left, right)} \\\\"
     ##########
     buffer_l = deepcopy(left)
     buffer_l.additional_Q()
     buffer_r = deepcopy(right)
     buffer_r.additional_Q()
-    commutator += f"\n &{sign[0]} \\Delta_\\text{{CDW}} \\left( {print_duo(buffer_l, right)} - {print_duo(left, buffer_r)} \\right) \\\\"
+    commutator += f"\n &{sign[0]} \\Delta_\\text{{CDW}} \\left( {str_duo(buffer_l, right)} - {str_duo(left, buffer_r)} \\right) \\\\"
     ##########
     buffer_l = deepcopy(left)
     buffer_r = deepcopy(right)
@@ -173,27 +220,27 @@ else:
     buffer_r.make_sc()
     commutator += f"\n &{sign[0]} \\Delta_\\text{{SC}} \\left( "
     if(left.spin):
-        commutator += f"{print_duo(buffer_l, right)}"
+        commutator += f"{str_duo(buffer_l, right)}"
     else:
-        commutator += f"{print_duo(right, buffer_l)}"
+        commutator += f"{str_duo(right, buffer_l)}"
     commutator += f" {sign[1]} "
     if(right.spin):
-        commutator += f"{print_duo(left, buffer_r)}"
+        commutator += f"{str_duo(left, buffer_r)}"
     else:
-        commutator += f"{print_duo(buffer_r, left)}"
+        commutator += f"{str_duo(buffer_r, left)}"
     commutator += f" \\right) \\\\"
     ##########
     buffer_l.additional_Q()
     buffer_r.additional_Q()
     commutator += f"\n &{sign[0]} \\Delta_\\eta^* "
     if(left.spin):
-        commutator += f"{print_duo(buffer_l, right)}"
+        commutator += f"{str_duo(buffer_l, right)}"
     else:
-        commutator += f"{print_duo(right, buffer_l)}"
+        commutator += f"{str_duo(right, buffer_l)}"
     commutator += f" {sign[1]} \\Delta_\\eta "
     if(right.spin):
-        commutator += f"{print_duo(left, buffer_r)}"
+        commutator += f"{str_duo(left, buffer_r)}"
     else:
-        commutator += f"{print_duo(buffer_r, left)}"
+        commutator += f"{str_duo(buffer_r, left)}"
 
 print(commutator)
