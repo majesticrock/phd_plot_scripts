4af75208c9368c23b8f561b3f43656dab54f3715
majesticrock <joshua.althueser@web.de>
2022-12-20 10:03:49 +0100

    Commuting and anticommuting stuff

--- auto_commute.py
+++ auto_commute.py
@@ -1,8 +1,7 @@
-from copy import deepcopy
 from methods_for_commute import *
 
 N_U  = np.array([Operator(Momentum("k", True, False), True,  True),   Operator(Momentum("k", True, False), True,  False)])
-N_D  = np.array([Operator(Momentum("k", True, False), False, True),   Operator(Momentum("k", True, False), False, False)])
+N_D  = np.array([Operator(Momentum("k", False, False), False, True),   Operator(Momentum("k", False, False), False, False)])
 SC   = np.array([Operator(Momentum("k", False, False), False, False), Operator(Momentum("k", True, False), True,  False)])
 SC_D = np.array([Operator(Momentum("k", False, False), False, False), Operator(Momentum("k", True, False), True,  False)])
 dagger_it(SC_D)
@@ -15,105 +14,19 @@ dagger_it(CDW_D_D)
 ETA   = np.array([Operator(Momentum("k", False, True), False, False), Operator(Momentum("k", True, False), True, False)])
 ETA_D = np.array([Operator(Momentum("k", False, True), False, False), Operator(Momentum("k", True, False), True, False)])
 dagger_it(ETA_D)
-left  = SC[0]
-right = SC[1]
 
 commuted_with_H = Expression(1, np.array([], dtype=Term))
-if(left.daggered == right.daggered):
-    if(left.daggered):
-        if(not left.spin):
-            b = right
-            right = deepcopy(left)
-            left = deepcopy(b)
-        else:
-            commuted_with_H.global_prefactor = -1
-    elif(left.spin):
-        b = right
-        right = deepcopy(left)
-        left = deepcopy(b)
-        commuted_with_H.global_prefactor = -1
+ex_l = Expression(1, np.array([ Term(1, "", N_U), Term(1, "", N_D) ]))
+ex_r = Expression(1, np.array([ Term(1, "", N_U), Term(1, "", N_D) ]))
 
-    commuted_with_H.append([ Term(*sync_eps(left.momentum), np.array([right, left])), Term(*sync_eps(left.momentum, -1), np.array([left, right])) ])
-    ##########
-    buffer_l = deepcopy(left)
-    buffer_l.additional_Q()
-    buffer_r = deepcopy(right)
-    buffer_r.additional_Q()
-    commuted_with_H.append([ Term(1, "\\Delta_\\text{CDW}", np.array([right, buffer_l])), Term(-1, "\\Delta_\\text{CDW}", np.array([left, buffer_r])) ])
-    ##########
-    buffer_l = deepcopy(left)
-    buffer_l.make_sc()
-    buffer_r = deepcopy(right)
-    buffer_r.make_sc()
-    commuted_with_H.append([ Term(1, "\\Delta_\\text{SC}", np.array([buffer_l, right])), Term(1, "\\Delta_\\text{SC}", np.array([buffer_r, left])) ])
-    if(left.momentum == buffer_r.momentum):
-        commuted_with_H.append(Term(-1, "\\Delta_\\text{SC}", np.array([], dtype=Operator)))
-    ##########
-    buffer_l = deepcopy(buffer_l)
-    buffer_r = deepcopy(buffer_r)
-    buffer_l.additional_Q()
-    buffer_r.additional_Q()
-    if(left.daggered):
-        commuted_with_H.append([ Term(1, "\\Delta_\\eta^*", np.array([buffer_l, right])), Term(1, "\\Delta_\\eta^*", np.array([buffer_r, left])) ])
-        if(left.momentum == buffer_r.momentum):
-            commuted_with_H.append(Term(-1, "\\Delta_eta^*", np.array([], dtype=Operator)))
-    else:
-        commuted_with_H.append([ Term(1, "\\Delta_\\eta", np.array([buffer_l, right])), Term(1, "\\Delta_\\eta", np.array([buffer_r, left])) ])
-        if(left.momentum == buffer_r.momentum):
-            commuted_with_H.append(Term(-1, "\\Delta_eta", np.array([], dtype=Operator)))
-else:
-    #ensure that we have a normal ordered term
-    if(right.daggered):
-        b = right
-        right = deepcopy(left)
-        left = deepcopy(b)
-        commuted_with_H.global_prefactor = -1
-        if(left.momentum == right.momentum and left.spin == right.spin):
-            commuted_with_H.append([Term(-1, "", np.array([], dtype=Operator))])
-
-    commuted_with_H.append([ Term(*sync_eps(left.momentum), np.array([left, right])), Term(*sync_eps(left.momentum, -1), np.array([left, right])) ])
-    ##########
-    buffer_l = deepcopy(left)
-    buffer_l.additional_Q()
-    buffer_r = deepcopy(right)
-    buffer_r.additional_Q()
-    commuted_with_H.append([ Term(1, "\\Delta_\\text{CDW}", np.array([buffer_l, right])), Term(-1, "\\Delta_\\text{CDW}", np.array([left, buffer_r])) ])
-    ##########
-    buffer_l = deepcopy(left)
-    buffer_r = deepcopy(right)
-    buffer_l.make_sc()
-    buffer_r.make_sc()
-
-    if(left.spin):
-        commuted_with_H.append([ Term(1, "\\Delta_\\text{SC}", np.array([buffer_l, right])) ])
-    else:
-        commuted_with_H.append([ Term(1, "\\Delta_\\text{SC}", np.array([right, buffer_l])) ])
-
-    if(right.spin):
-        commuted_with_H.append([ Term(-1, "\\Delta_\\text{SC}", np.array([left, buffer_r])) ])
-    else:
-        commuted_with_H.append([ Term(-1, "\\Delta_\\text{SC}", np.array([buffer_r, left])) ])
-
-    ##########
-    buffer_l = deepcopy(buffer_l)
-    buffer_r = deepcopy(buffer_r)
-    buffer_l.additional_Q()
-    buffer_r.additional_Q()
-    if(left.spin):
-        commuted_with_H.append([ Term(1, "\\Delta_\\eta^*", np.array([buffer_l, right])) ])
-    else:
-        commuted_with_H.append([ Term(1, "\\Delta_\\eta^*", np.array([right, buffer_l])) ])
-
-    if(right.spin):
-        commuted_with_H.append([ Term(1, "\\Delta_\\eta", np.array([left, buffer_r])) ])
-    else:
-        commuted_with_H.append([ Term(1, "\\Delta_\\eta", np.array([buffer_r, left])) ])
+for i in range(0, ex_r.terms.size):
+    left  = ex_r.terms[i].operators[0]
+    right = ex_r.terms[i].operators[1]
+    commute_bilinear_with_H(left, right, commuted_with_H)
 
 commuted_with_H.normalOrder()
-print(f"\\left[H, {str_duo(left, right)}\\right] &= {commuted_with_H}\n")
+print(f"\\left[H, {ex_r}\\right] &= {commuted_with_H}\n")
 
-ex_l = Expression(1, np.array([ Term(1, "", SC_D) ]))
-ex_r = Expression(1, np.array([ Term(1, "", SC) ]))
 c = anti_commmute(ex_l, commuted_with_H)
 c.normalOrder()
 print(f"\\left\\{{ {ex_l}, \\left[ H, {ex_r} \\right] \\right\\}} &= {c}")
--- methods_for_commute.py
+++ methods_for_commute.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
+from copy import deepcopy
 import numpy as np
 
 def str_duo(l, r):
     if(l.daggered and not r.daggered):
         if(l.spin == r.spin):
-            if(l.momentum.value == r.momentum.value and l.momentum.positve == r.momentum.positve):
+            if(l.momentum.value == r.momentum.value and l.momentum.positive == r.momentum.positive):
                 if(l.momentum.add_Q == r.momentum.add_Q):
                     ret = f"n_{{{l.momentum}{l.spin_as_string()}}}"
                 else:
@@ -17,7 +18,7 @@ def str_duo(l, r):
     
     else:
         if(l.spin != r.spin):
-            if(l.momentum.value == r.momentum.value and l.momentum.positve != r.momentum.positve):
+            if(l.momentum.value == r.momentum.value and l.momentum.positive != r.momentum.positive):
                 if(not r.daggered):
                     if(r.spin):
                         if(l.momentum.add_Q == r.momentum.add_Q):
@@ -48,16 +49,16 @@ def str_duo(l, r):
 @dataclass
 class Momentum:
     value: str
-    positve: bool
+    positive: bool
     add_Q: bool
 
     def __str__(self):
         ret = ""
-        if(not self.positve):
+        if(not self.positive):
             ret += "-"
         ret += self.value
         if(self.add_Q):
-            if(self.positve):
+            if(self.positive):
                 ret += "+"
             else:
                 ret += "-"
@@ -89,7 +90,7 @@ class Operator:
     
     def make_sc(self):
         self.spin = not self.spin
-        self.momentum.positve = not self.momentum.positve
+        self.momentum.positive = not self.momentum.positive
         self.daggered = not self.daggered
 
     def spin_as_string(self):
@@ -157,6 +158,37 @@ class Expression:
     def append(self, values):
         self.terms = np.append(self.terms, values)
 
+    def sortTerms(self):
+        for t in self.terms:
+            for i in range(0, t.operators.size):
+                for j in range(i + 1, t.operators.size):
+                    if(t.operators[i].daggered == t.operators[j].daggered):
+                        if(not t.operators[i].daggered):
+                            if(t.operators[i].spin and not t.operators[j].spin):
+                                t.operators[i], t.operators[j] = t.operators[j], t.operators[i]
+                                if((j - i) % 2 == 1):
+                                    t.prefactor *= -1
+                        else:
+                            if(not t.operators[i].spin and t.operators[j].spin):
+                                t.operators[i], t.operators[j] = t.operators[j], t.operators[i]
+                                if((j - i) % 2 == 1):
+                                    t.prefactor *= -1
+            for i in range(0, t.operators.size):
+                for j in range(i + 1, t.operators.size):
+                    if(t.operators[i].daggered == t.operators[j].daggered and t.operators[i].spin == t.operators[j].spin):
+                        if(t.operators[i].momentum.add_Q and not t.operators[j].momentum.add_Q):
+                            t.operators[i], t.operators[j] = t.operators[j], t.operators[i]
+                            if((j - i) % 2 == 1):
+                                t.prefactor *= -1
+            
+            for i in range(0, t.operators.size):
+                for j in range(i + 1, t.operators.size):
+                    if(t.operators[i].daggered == t.operators[j].daggered and t.operators[i].spin == t.operators[j].spin and t.operators[i].momentum.add_Q == t.operators[j].momentum.add_Q):
+                        if(not t.operators[i].momentum.positive and t.operators[j].momentum.positive):
+                            t.operators[i], t.operators[j] = t.operators[j], t.operators[i]
+                            if((j - i) % 2 == 1):
+                                t.prefactor *= -1
+
     def normalOrder(self):
         t = 0
         while t < self.terms.size:
@@ -178,20 +210,8 @@ class Expression:
                 n = new_n
             t += 1
 
-        for t in self.terms:
-            for i in range(0, t.operators.size):
-                for j in range(i + 1, t.operators.size):
-                    if(t.operators[i].daggered == t.operators[j].daggered):
-                        if(not t.operators[i].daggered):
-                            if(t.operators[i].spin and not t.operators[j].spin):
-                                t.operators[i], t.operators[j] = t.operators[j], t.operators[i]
-                                if((j - i) % 2 == 1):
-                                    t.prefactor *= -1
-                        else:
-                            if(not t.operators[i].spin and t.operators[j].spin):
-                                t.operators[i], t.operators[j] = t.operators[j], t.operators[i]
-                                if((j - i) % 2 == 1):
-                                    t.prefactor *= -1
+        self.sortTerms()
+
         i = 0
         while i < self.terms.size:
             skip = False
@@ -224,6 +244,102 @@ def dagger_it(src):
     src[0].daggered = not src[0].daggered
     src[1].daggered = not src[1].daggered
 
+def commute_bilinear_with_H(left: Operator, right: Operator, commuted_with_H: Expression):
+    if(left.daggered == right.daggered):
+        if(left.daggered):
+            if(not left.spin):
+                b = right
+                right = deepcopy(left)
+                left = deepcopy(b)
+            else:
+                commuted_with_H.global_prefactor = -1
+        elif(left.spin):
+            b = right
+            right = deepcopy(left)
+            left = deepcopy(b)
+            commuted_with_H.global_prefactor = -1
+
+        commuted_with_H.append([ Term(*sync_eps(left.momentum), np.array([right, left])), Term(*sync_eps(left.momentum, -1), np.array([left, right])) ])
+        ##########
+        buffer_l = deepcopy(left)
+        buffer_l.additional_Q()
+        buffer_r = deepcopy(right)
+        buffer_r.additional_Q()
+        commuted_with_H.append([ Term(1, "\\Delta_\\text{CDW}", np.array([right, buffer_l])), Term(-1, "\\Delta_\\text{CDW}", np.array([left, buffer_r])) ])
+        ##########
+        buffer_l = deepcopy(left)
+        buffer_l.make_sc()
+        buffer_r = deepcopy(right)
+        buffer_r.make_sc()
+        commuted_with_H.append([ Term(1, "\\Delta_\\text{SC}", np.array([buffer_l, right])), Term(1, "\\Delta_\\text{SC}", np.array([buffer_r, left])) ])
+        if(left.momentum == buffer_r.momentum):
+            commuted_with_H.append(Term(-1, "\\Delta_\\text{SC}", np.array([], dtype=Operator)))
+        ##########
+        buffer_l = deepcopy(buffer_l)
+        buffer_r = deepcopy(buffer_r)
+        buffer_l.additional_Q()
+        buffer_r.additional_Q()
+        if(left.daggered):
+            commuted_with_H.append([ Term(1, "\\Delta_\\eta^*", np.array([buffer_l, right])), Term(1, "\\Delta_\\eta^*", np.array([buffer_r, left])) ])
+            if(left.momentum == buffer_r.momentum):
+                commuted_with_H.append(Term(-1, "\\Delta_eta^*", np.array([], dtype=Operator)))
+        else:
+            commuted_with_H.append([ Term(1, "\\Delta_\\eta", np.array([buffer_l, right])), Term(1, "\\Delta_\\eta", np.array([buffer_r, left])) ])
+            if(left.momentum == buffer_r.momentum):
+                commuted_with_H.append(Term(-1, "\\Delta_eta", np.array([], dtype=Operator)))
+    else:
+        #ensure that we have a normal ordered term
+        if(right.daggered):
+            b = right
+            right = deepcopy(left)
+            left = deepcopy(b)
+            commuted_with_H.global_prefactor = -1
+            if(left.momentum == right.momentum and left.spin == right.spin):
+                commuted_with_H.append([Term(-1, "", np.array([], dtype=Operator))])
+
+        commuted_with_H.append([ Term(*sync_eps(left.momentum), np.array([left, right])), Term(*sync_eps(left.momentum, -1), np.array([left, right])) ])
+        ##########
+        buffer_l = deepcopy(left)
+        buffer_l.additional_Q()
+        buffer_r = deepcopy(right)
+        buffer_r.additional_Q()
+        commuted_with_H.append([ Term(1, "\\Delta_\\text{CDW}", np.array([buffer_l, right])), Term(-1, "\\Delta_\\text{CDW}", np.array([left, buffer_r])) ])
+        ##########
+        buffer_l = deepcopy(left)
+        buffer_r = deepcopy(right)
+        buffer_l.make_sc()
+        buffer_r.make_sc()
+
+        if(left.spin):
+            commuted_with_H.append([ Term(1, "\\Delta_\\text{SC}", np.array([buffer_l, right])) ])
+        else:
+            commuted_with_H.append([ Term(1, "\\Delta_\\text{SC}", np.array([right, buffer_l])) ])
+
+        if(right.spin):
+            commuted_with_H.append([ Term(-1, "\\Delta_\\text{SC}", np.array([left, buffer_r])) ])
+        else:
+            commuted_with_H.append([ Term(-1, "\\Delta_\\text{SC}", np.array([buffer_r, left])) ])
+
+        ##########
+        buffer_l = deepcopy(buffer_l)
+        buffer_r = deepcopy(buffer_r)
+        buffer_l.additional_Q()
+        buffer_r.additional_Q()
+        if(left.spin):
+            commuted_with_H.append([ Term(1, "\\Delta_\\eta^*", np.array([buffer_l, right])) ])
+        else:
+            commuted_with_H.append([ Term(1, "\\Delta_\\eta^*", np.array([right, buffer_l])) ])
+
+        if(right.spin):
+            commuted_with_H.append([ Term(-1, "\\Delta_\\eta", np.array([left, buffer_r])) ])
+        else:
+            commuted_with_H.append([ Term(-1, "\\Delta_\\eta", np.array([buffer_r, left])) ])
+
+    if(commuted_with_H.global_prefactor != 1):
+        for t in commuted_with_H.terms:
+            t.prefactor *= commuted_with_H.global_prefactor
+        commuted_with_H.global_prefactor = 1
+
 def anti_commmute(l: Expression, r: Expression):
     commuted = Expression(l.global_prefactor * r.global_prefactor, np.array([], dtype=Term))
     for lt in l.terms:
@@ -232,7 +348,6 @@ def anti_commmute(l: Expression, r: Expression):
             commuted.append([ Term(lt.prefactor * rt.prefactor, lt.coefficient + rt.coefficient, np.append(rt.operators, lt.operators)) ])
 
     return commuted
-
 
 def commmute(l: Expression, r: Expression):
     commuted = Expression(l.global_prefactor * r.global_prefactor, np.array([], dtype=Term))
