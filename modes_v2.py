29d46ef65c1453731542df1af75c000a57bb9be3
majesticrock <joshua.althueser@web.de>
2023-02-05 22:06:37 +0100

    ss

--- modes_v2.py
+++ modes_v2.py
@@ -1,22 +1,22 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-nameU = "-3.00"
+nameU = "-5.00"
 
 M = np.loadtxt(f"data/T0.1/U_modes/{nameU}.txt").transpose()
-M = 1 / M
+#M = 1 / M
 
 N = np.loadtxt(f"data/T0.1/U_modes/{nameU}_one_particle.txt").transpose().flatten()
 
 M.sort()
 N.sort()
 
-sigma = 0.05
+sigma = 0.03
 def gauss(x, mu):
     return np.exp((-(x - mu)**2) / (2*sigma)) / np.sqrt(2*np.pi*sigma)
 
 lims = [min(M) - 1, max(M) + 1]
-lims = [-12, 12]
+#lims = [-12, 12]
 size = M.size
 x_lin = np.linspace(lims[0], lims[1], size)
 
@@ -28,11 +28,16 @@ data /= max(data)
 data *= 16
 
 plt.plot(x_lin, data, label="DoS")
-plt.plot(x_lin, M, "x", label="Two Particle")
+plt.plot(M, np.linspace(min(data), max(data), len(M)), "x", label="Spectrum")
 #plt.plot(x_lin, N, "o", mfc="none", label="One Particle")
 
 #plt.xlim(lims[0], lims[1])
 #plt.ylim(-20, 20)
-plt.xlabel("$\\epsilon /t$")
+plt.xlabel(r"$\epsilon / t$")
+plt.ylabel(r"DoS /  a.u.")
 plt.legend()
+plt.tight_layout()
+
+import os
+plt.savefig(f"python/build/{os.path.basename(__file__).split('.')[0]}_U={nameU}.pdf")
 plt.show()
