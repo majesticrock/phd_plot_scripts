a2617c2ac8ece421a5f7499cc1b80cdfa89e03b8
majesticrock <joshua.althueser@web.de>
2023-02-24 15:50:39 +0100

    G hat negative Diagonalelemente

--- modes_v2.py
+++ modes_v2.py
@@ -8,12 +8,14 @@ M = np.loadtxt(f"data/T0.1/U_modes/{nameU}.txt").transpose()
 
 N = np.loadtxt(f"data/T0.1/U_modes/{nameU}_one_particle.txt").transpose().flatten()
 
-M.sort()
-for i in range(0,len(M)):
-    if(abs(M[i]) < 1e-10):
-        M[i] = 1e15
-    
+
+#for i in range(0,len(M)):
+#    if(abs(M[i]) < 1e-10):
+#        M[i] = 1e15
+M = M[abs(M) > 1e-3]
+   
 M = 1/M
+M.sort()
 N.sort()
 
 sigma = 0.03
