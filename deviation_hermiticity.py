8d38b4374ee250707a0184d0a5c7ffda92d258f1
majesticrock <joshua.althueser@googlemail.com>
2023-03-08 12:14:37 +0100

    hermitian deviation update

--- deviation_hermiticity.py
+++ deviation_hermiticity.py
@@ -3,12 +3,15 @@ import matplotlib.pyplot as plt
 
 M = np.loadtxt("data/deviations_hermiticity.csv", delimiter=";").transpose()
 
-plt.plot(M[0], M[1], "-o")
+plt.plot(M[0], M[1], linestyle="-", marker="o", markersize=6, label="$U=-1$")
+plt.plot(M[0], M[2], linestyle="-", marker="s", markersize=6, label="$U=-2$")
+plt.plot(M[0], M[3], linestyle="-", marker="*", markersize=10, label="$U=-3$")
 plt.yscale("log")
 
-plt.text(10, 0.1, r"$\epsilon_0(k) = -\frac{U}{2L^2} \cdot \sum_q \frac{\langle g_q \rangle}{\langle g_k \rangle} (1 - 2\langle n_k \rangle)$", fontsize=20)
+#plt.text(10, 0.01, r"$\epsilon_0(k) = -\frac{U}{2L^2} \cdot \sum_q \frac{\langle g_q \rangle}{\langle g_k \rangle} (1 - 2\langle n_k \rangle)$", fontsize=20)
 plt.xlabel(r"$L$")
-plt.ylabel(r"Deviation")
+plt.ylabel(r"$\Delta_H$")
+plt.legend()
 plt.tight_layout()
 
 import os
