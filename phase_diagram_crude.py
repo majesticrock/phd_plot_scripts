54ed220a5b3f25dfd60fbeb2a3a1c629803ce4c6
majesticrock <joshua.althueser@web.de>
2023-06-12 09:34:04 +0200

    Cluster2

--- phase_diagram_crude.py
+++ phase_diagram_crude.py
@@ -9,8 +9,10 @@ if(len(sys.argv) > 1):
     data_folder = "data/" + sys.argv[1] + "/"
     name = sys.argv[1]
 else:
-    name = "T0"
+    name = "T0_L80"
     data_folder = f"data/phases/{name}/"
+
+swapAxis = False
 
 def pair_sort(pair_arr, sortBy):
     if len(pair_arr) < 2: 
@@ -22,43 +24,23 @@ def pair_sort(pair_arr, sortBy):
                 pair_arr[sortBy][i], pair_arr[sortBy][j] = pair_arr[sortBy][j], pair_arr[sortBy][i]
                 pair_arr[1 - sortBy][i], pair_arr[1 - sortBy][j] = pair_arr[1 - sortBy][j], pair_arr[1 - sortBy][i]
 
-with gzip.open(data_folder + "cdw.dat.gz", 'rt') as f_open:
-    CDW = np.loadtxt(f_open)
-with gzip.open(data_folder + "afm.dat.gz", 'rt') as f_open:
-    AFM = np.loadtxt(f_open)
-with gzip.open(data_folder + "sc.dat.gz", 'rt') as f_open:
-    SC =  abs(np.loadtxt(f_open))
-with gzip.open(data_folder + "gamma_sc.dat.gz", 'rt') as f_open:
-    GAMMA_SC = abs(np.loadtxt(f_open))
-with gzip.open(data_folder + "xi_sc.dat.gz", 'rt') as f_open:
-    XI_SC = abs(np.loadtxt(f_open))
-with gzip.open(data_folder + "eta.dat.gz", 'rt') as f_open:
-    ETA = abs(np.loadtxt(f_open))
+file_names = np.array(["cdw", "afm", "sc", "xi_sc"])
+crudeData = []
+boundData = []
 
+for name in file_names:
+    with gzip.open(data_folder + f"{name}.dat.gz", 'rt') as f_open:
+        if swapAxis:
+            crudeData.append(np.loadtxt(f_open).transpose())
+        else:
+            crudeData.append(np.loadtxt(f_open))
 
-with gzip.open(data_folder + "boundaries_cdw.dat.gz", 'rt') as f_open:
-    BOUND_CDW = np.loadtxt(f_open)
-    pair_sort(BOUND_CDW, 0)
-with gzip.open(data_folder + "boundaries_afm.dat.gz", 'rt') as f_open:
-    BOUND_AFM = np.loadtxt(f_open)
-    pair_sort(BOUND_AFM, 0)
-with gzip.open(data_folder + "boundaries_sc.dat.gz", 'rt') as f_open:
-    BOUND_SC = np.loadtxt(f_open)
-    pair_sort(BOUND_SC, 0)
-with gzip.open(data_folder + "boundaries_gamma_sc.dat.gz", 'rt') as f_open:
-    BOUND_GAMMA_SC = np.loadtxt(f_open)
-    pair_sort(BOUND_GAMMA_SC, 0)
-with gzip.open(data_folder + "boundaries_xi_sc.dat.gz", 'rt') as f_open:
-    BOUND_XI_SC = np.loadtxt(f_open)
-    pair_sort(BOUND_XI_SC, 1)
-with gzip.open(data_folder + "boundaries_eta.dat.gz", 'rt') as f_open:
-    BOUND_ETA = np.loadtxt(f_open)
-    pair_sort(BOUND_ETA, 0)
-
+    with gzip.open(data_folder + f"boundaries_{name}.dat.gz", 'rt') as f_open:
+        boundData.append(np.loadtxt(f_open))
 
 labels = ["T", "U"]
-T_SIZE = len(CDW)
-U_SIZE = len(CDW[0])
+T_SIZE = len(crudeData[0])
+U_SIZE = len(crudeData[0][0])
 
 with gzip.open(data_folder + "cdw.dat.gz", 'rt') as fp:
     for i, line in enumerate(fp):
@@ -73,76 +55,42 @@ with gzip.open(data_folder + "cdw.dat.gz", 'rt') as fp:
         elif i > 3:
             break
 
-for i in range(0, T_SIZE):
-    for j in range(0, U_SIZE):
-        if(CDW[i][j] > eps):
-            CDW[i][j] = 1
-        else:
-            CDW[i][j] = 0
-
-        if(AFM[i][j] > eps):
-            AFM[i][j] = 1
-        else:
-            AFM[i][j] = 0
-
-        if(SC[i][j] > eps):
-            SC[i][j] = 1
-        else:
-            SC[i][j] = 0
-
-        if(GAMMA_SC[i][j] > eps):
-            GAMMA_SC[i][j] = 1
-        else:
-            GAMMA_SC[i][j] = 0
-
-        if(XI_SC[i][j] > eps):
-            XI_SC[i][j] = 1
-        else:
-            XI_SC[i][j] = 0
-
-        if(ETA[i][j] > eps):
-            ETA[i][j] = 1
-        else:
-            ETA[i][j] = 0
+for k in range(0, len(file_names)):
+    for i in range(0, T_SIZE):
+        for j in range(0, U_SIZE):
+            if(crudeData[k][i][j] > eps):
+                crudeData[k][i][j] = 1
+            else:
+                crudeData[k][i][j] = 0
 
 
 X, Y = np.meshgrid(U, T)
-cmap0 = colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C0', 0.5)])
-cmap1 = colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C1', 0.5)])
-cmap2 = colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C2', 0.5)])
-cmap3 = colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C3', 0.5)])
-cmap4 = colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C4', 0.5)])
+cmaps = []
+cmaps.append(colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C0', 0.5)]))
+cmaps.append(colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C1', 0.5)]))
+cmaps.append(colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C2', 0.5)]))
+cmaps.append(colors.ListedColormap([colors.to_rgba('white', 0), colors.to_rgba('C3', 0.5)]))
 
 fig, ax = plt.subplots()
 
-cset0 = ax.contourf(X, Y, SC, 1, cmap=cmap0)
-cset1 = ax.contourf(X, Y, CDW, 1, cmap=cmap1)
-cset2 = ax.contourf(X, Y, AFM, 1, cmap=cmap2)
-cset3 = ax.contourf(X, Y, XI_SC, 1, cmap=cmap3)
-#cset4 = ax.contourf(X, Y, GAMMA_SC, 1, cmap=cmap4)
-#cbar = fig.colorbar(cset1)
+for i in range(0, len(file_names)):
+    ax.contourf(X, Y, crudeData[i], 1, cmap=cmaps[i])
 
 from matplotlib.patches import Patch
 
-legend_elements = [Patch(facecolor='C0', label=r'$s$'),
-            Patch(facecolor='C1', label=r'CDW'),
-            Patch(facecolor='C2', label=r'AFM'),
+legend_elements = [Patch(facecolor='C0', label=r'CDW'),
+            Patch(facecolor='C1', label=r'AFM'),
+            Patch(facecolor='C2', label=r'$s$'),
             Patch(facecolor='C3', label=r'$d_{x^2 - y^2}$')]
             #,Patch(facecolor='C4', label=r'$\tilde{s}$')]
 ax.legend(handles=legend_elements, loc='upper left')
 
-if(len(BOUND_CDW) == 2):
-    ax.plot(BOUND_CDW[1], BOUND_CDW[0], "k.")
-if(len(BOUND_AFM) == 2):
-    ax.plot(BOUND_AFM[1], BOUND_AFM[0], "k.")
-if(len(BOUND_SC) == 2):
-    ax.plot(BOUND_SC[1], BOUND_SC[0], "k.")
-if(len(BOUND_GAMMA_SC) == 2):
-    ax.plot(BOUND_GAMMA_SC[1], BOUND_GAMMA_SC[0], "k.")
-if(len(BOUND_XI_SC) == 2):
-    ax.plot(BOUND_XI_SC[1], BOUND_XI_SC[0], "k.")
-if(len(BOUND_ETA) == 2):
-    ax.plot(BOUND_ETA[1], BOUND_ETA[0], "k.")
+for i in range(0, len(file_names)):
+    if len(boundData[i]) == 2:
+        if swapAxis:
+            ax.plot(boundData[i][0], boundData[i][1], "k.")
+        else:
+            ax.plot(boundData[i][1], boundData[i][0], "k.")
 
 
 micnas_d   =  np.loadtxt("data/micnas_d_wave.csv").transpose()
