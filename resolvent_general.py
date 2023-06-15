c1d847111d0ac0829700e9a23fa3b56dcfd94bd3
majesticrock <joshua.althueser@web.de>
2023-06-15 16:05:47 +0200

    Jetzt Matrizen komplex machen

--- resolvent_general.py
+++ resolvent_general.py
@@ -11,13 +11,13 @@ Us = np.array([-2.0])
 Vs = np.array([-0.1])
 
 folder = "data/test/"
-name_suffix = "SC"
+name_suffix = "cdw_"
+element_names = ["a", "a+b", "a+ib"]
 fig, ax = plt.subplots()
 
 #ax.set_xscale("log")
 ax.set_yscale("log")
 
-types = [ "phase"]#"higgs", 
 lss = ["-", "--", "-."]
 
 plot_upper_lim = 8.5
@@ -34,32 +34,33 @@ for q, T in enumerate(Ts):
             roots = np.array([np.min(one_particle) * 2, np.max(one_particle) * 2])**2
             a_inf = (roots[0] + roots[1]) * 0.5
             b_inf = ((roots[1] - roots[0]) * 0.25)
+            ax.axvspan(np.sqrt(roots[1]), np.sqrt(roots[0]), alpha=.2, color="purple", label="Continuum")
 
-            #ax.axvspan(np.sqrt(roots[1]), np.sqrt(roots[0]), alpha=.2, color="purple", label="Continuum")
-            for idx, type in enumerate(types):
-                file = f"{folder}{name}resolvent_{type}_{name_suffix}.dat.gz"
+            def terminator(w):
+                p = w - a_inf
+                q = 4 * b_inf**2
+                root = np.sqrt(np.real(p**2 - q), dtype=complex)
+                return_arr = np.zeros(len(w), dtype=complex)
+                for i in range(0, len(w)):
+                    if(w[i] > roots[0]):
+                        return_arr[i] = (p[i] - root[i]) / (2. * b_inf**2)
+                    else:
+                        return_arr[i] = (p[i] + root[i]) / (2. * b_inf**2)
+                return return_arr
+
+            w_vals = 20000
+            w_lin = np.linspace(0, plot_upper_lim, w_vals, dtype=complex)**2
+            w_lin += 1e-8j
+            off = 1
+
+            data = np.zeros(w_vals)
+
+            for idx, element in enumerate(element_names):
+                file = f"{folder}{name}resolvent_{name_suffix}{element}.dat.gz"
                 with gzip.open(file, 'rt') as f_open:
                     M = np.loadtxt(f_open)
                     A = M[0]
                     B = M[1]
-
-                w_vals = 20000
-                w_lin = np.linspace(0, plot_upper_lim, w_vals, dtype=complex)**2
-                w_lin += 1e-8j
-                off = 1
-
-                def terminator(w):
-                    p = w - a_inf
-                    q = 4 * b_inf**2
-                    root = np.sqrt(np.real(p**2 - q), dtype=complex)
-                    return_arr = np.zeros(len(w), dtype=complex)
-                    for i in range(0, len(w)):
-                        if(w[i] > roots[0]):
-                            return_arr[i] = (p[i] - root[i]) / (2. * b_inf**2)
-                        else:
-                            return_arr[i] = (p[i] + root[i]) / (2. * b_inf**2)
-
-                    return return_arr
 
                 deviation_from_inf = np.zeros(len(A) - 1)
                 for i in range(0, len(A) - 1):
@@ -71,18 +72,20 @@ for q, T in enumerate(Ts):
                     for i in range(0, len(w)):
                         if(w[i].real > roots[0] and w[i].real < roots[1]):
                             w[i] = w[i].real
-
                     G = w - A[len(A) - off_termi] - B[len(B) - off_termi] * terminator( w )
                     for j in range(len(A) - off_termi - 1, -1, -1):
                         G = w - A[j] - B[j + 1] / G
                     return B[0] / G
+                
+                if idx == 0:
+                    data -= dos( np.copy(w_lin) ).imag
+                elif idx == 1:
+                    data -= dos( np.copy(w_lin) ).imag
+                elif idx == 2:
+                    data += dos( np.copy(w_lin) ).imag
 
-                if(idx == 0):
-                    ax.plot(np.sqrt(w_lin.real), -dos( np.copy(w_lin) ).imag, color=colors[q+r+s],
-                        linestyle=lss[idx], linewidth=(plt.rcParams["lines.linewidth"]+idx*2), label=f"$V={V}$")
-                else:
-                    ax.plot(np.sqrt(w_lin.real), -dos( np.copy(w_lin) ).imag, color=colors[q+r+s],
-                        linestyle=lss[idx], linewidth=(plt.rcParams["lines.linewidth"]+idx*2))
+            ax.plot(np.sqrt(w_lin.real), data, color=colors[q+r+s],
+                linewidth=(plt.rcParams["lines.linewidth"]), label=f"$V={V}$")
 
 legend = plt.legend()
 
