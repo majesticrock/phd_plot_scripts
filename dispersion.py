a0ea722e6fe2b7c799c4e8e56e204a477b1510ab
Joshua <joshua.althueser@gmail.com>
2023-09-11 16:05:49 +0200

    Eigen's [] operator scheint seltsame Dinge zu machen

--- dispersion.py
+++ dispersion.py
@@ -1,12 +1,12 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import gzip
 
-file = "data/energies.txt"
-M = np.loadtxt(file).transpose()
+file = "data/modes/square/test/T=0.0/U=-2.0_V=-0.1/one_particle.dat.gz"
+with gzip.open(file, 'rt') as f_open:
+    M = np.loadtxt(f_open).transpose()
 
-linestyles = ["-", "--"]
-for i in range(0, len(M)):
-    plt.plot(M[i], linestyle=linestyles[i % 2])
+plt.plot(M)
 
 plt.ylabel(r"$\epsilon / t$")
 plt.xlabel(r"$k / \pi$")
