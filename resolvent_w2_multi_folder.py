65df8ca816273d1372b84134687574ff08b6b932
majesticrock <joshua.althueser@googlemail.com>
2023-10-11 11:15:08 +0200

    More data

--- resolvent_w2_multi_folder.py
+++ resolvent_w2_multi_folder.py
@@ -9,13 +9,13 @@ colors = prop_cycle.by_key()['color']
 
 Ts = np.array([0.])
 Us = np.array([-2.0])
-Vs = np.array([0.25, 0.1, 0.05, 0.01, 0.005, 0.001, 0])
+Vs = np.array(["0.25", "0.1", "0.05", "0.01", "0.005", "0.001", "0.0005", "0.0001", "0.00005", "0.0"])
 
 folder = "data/modes/square/dos_900/"
 element_names = ["a", "a+b", "a+ib"]
 fig, ax = plt.subplots()
 
-#cax.set_xscale("log")
+#ax.set_xscale("log")
 #ax.set_yscale("log")
 
 plot_upper_lim = 8.5
@@ -26,17 +26,19 @@ counter = 0
 for T, U, V in iterate_containers(Ts, Us, Vs):
     name = f"T={T}/U={U}_V={V}"
     data, data_real, w_lin, res = cf.resolvent_data(f"{folder}{name}", name_suffix, 1e-4, plot_upper_lim, number_of_values=30000, imaginary_offset=1e-6)
-    #ax.plot(w_lin, data_real, linewidth=(plt.rcParams["lines.linewidth"]), linestyle="--", label=f"$V={V}$")
+    #ax.plot(w_lin, data, linewidth=(plt.rcParams["lines.linewidth"]), linestyle="--", label=f"$V={V}$")
     
     peak_positions[counter] = w_lin[np.argmax(data)]
     counter += 1
 
-ax.plot(Vs, peak_positions)
+ax.plot(np.array([float(v) for v in Vs]), peak_positions, "x")
+ax.set_xlabel(r"$V / t$")
+ax.set_ylabel(r"Peak position $z/ t$")
 #res.mark_continuum(ax)
 #legend = plt.legend(loc=8)
 
-ax.set_xlabel(r"$z / t$")
-ax.set_ylabel(r"Spectral density / a.u.")
+#ax.set_xlabel(r"$z / t$")
+#ax.set_ylabel(r"Spectral density / a.u.")
 fig.tight_layout()
 
 import os
