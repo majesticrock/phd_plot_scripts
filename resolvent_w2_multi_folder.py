4d807a2a47f7d49f6b13133bf34c9578cac50e94
majesticrock <joshua.althueser@googlemail.com>
2023-10-11 09:49:45 +0200

    ws

--- resolvent_w2_multi_folder.py
+++ resolvent_w2_multi_folder.py
@@ -15,19 +15,25 @@ folder = "data/modes/square/dos_900/"
 element_names = ["a", "a+b", "a+ib"]
 fig, ax = plt.subplots()
 
-ax.set_xscale("log")
-ax.set_yscale("log")
+#cax.set_xscale("log")
+#ax.set_yscale("log")
 
 plot_upper_lim = 8.5
+name_suffix = "phase_SC"
 
-name_suffix = "higgs_SC"
+peak_positions = np.zeros(len(Vs))
+counter = 0
 for T, U, V in iterate_containers(Ts, Us, Vs):
     name = f"T={T}/U={U}_V={V}"
     data, data_real, w_lin, res = cf.resolvent_data(f"{folder}{name}", name_suffix, 1e-4, plot_upper_lim, number_of_values=30000, imaginary_offset=1e-6)
-    ax.plot(w_lin, data, linewidth=(plt.rcParams["lines.linewidth"]), linestyle="--", label=f"$V={V}$")
+    #ax.plot(w_lin, data_real, linewidth=(plt.rcParams["lines.linewidth"]), linestyle="--", label=f"$V={V}$")
+    
+    peak_positions[counter] = w_lin[np.argmax(data)]
+    counter += 1
 
+ax.plot(Vs, peak_positions)
 #res.mark_continuum(ax)
-legend = plt.legend(loc=8)
+#legend = plt.legend(loc=8)
 
 ax.set_xlabel(r"$z / t$")
 ax.set_ylabel(r"Spectral density / a.u.")
