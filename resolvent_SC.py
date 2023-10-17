67df02cbf29a5399174a656f431ddeee81a8eec8
majesticrock <joshua.althueser@googlemail.com>
2023-10-17 15:50:49 +0200

    uni

---
+++ resolvent_SC.py
@@ -0,0 +1,56 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import lib.continued_fraction as cf
+from lib.iterate_containers import iterate_containers
+# Calculates the resolvent in w^2
+
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
+
+Ts = np.array([0.1])
+Us = np.array([-2.0])
+Vs = np.array([-0.1])
+
+use_XP = True
+
+folder = "data/modes/square/dos_900/"
+name_suffix = "phase_SC"
+element_names = ["a", "a+b", "a+ib"]
+fig, ax = plt.subplots()
+
+#ax.set_xscale("log")
+ax.set_yscale("log")
+
+plot_lower_lim = 0
+plot_upper_lim = 15
+
+for T, U, V in iterate_containers(Ts, Us, Vs):
+    name = f"T={T}/U={U}_V={V}"
+    data, data_real, w_lin, res = cf.resolvent_data(f"{folder}{name}", name_suffix, plot_lower_lim, plot_upper_lim, number_of_values=20000, xp_basis=use_XP)
+    ax.plot(w_lin, data, linewidth=(plt.rcParams["lines.linewidth"]), label=name_suffix)
+
+name_suffix = "higgs_SC"
+for T, U, V in iterate_containers(Ts, Us, Vs):
+    name = f"T={T}/U={U}_V={V}"
+    data, data_real, w_lin, res = cf.resolvent_data(f"{folder}{name}", name_suffix, plot_lower_lim, plot_upper_lim, number_of_values=20000, xp_basis=use_XP)
+    ax.plot(w_lin, data, linewidth=(plt.rcParams["lines.linewidth"]), linestyle="--", label=name_suffix)
+
+res.mark_continuum(ax)
+legend = plt.legend()
+
+#import matplotlib.lines as mlines
+#dummy_lines = []
+#dummy_lines.append(mlines.Line2D([],[], color="k", linestyle="-"))
+#dummy_lines.append(mlines.Line2D([],[], color="k", linestyle="--", linewidth=2*plt.rcParams["lines.linewidth"]))
+#legend_extra = plt.legend([dummy_lines[i] for i in [0,1]], [r"Amplitude", r"Phase"], loc="upper center")
+
+ax.add_artist(legend)
+#ax.add_artist(legend_extra)
+
+ax.set_xlabel(r"$z / t$")
+ax.set_ylabel(r"Spectral density / a.u.")
+fig.tight_layout()
+
+import os
+plt.savefig(f"python/build/{os.path.basename(__file__).split('.')[0]}.pdf")
+plt.show()
--- resolvent_w2_multi_folder.py
+++ resolvent_w2_multi_folder.py
@@ -11,9 +11,9 @@ colors = prop_cycle.by_key()['color']
 realPart = False
 both = False
 
-Ts = np.array([0.])
+Ts = np.array([0., 0.1])
 Us = np.array([-2.0])
-Vs = np.array(["0.002", "0.001", "0.0005", "0.0001", "0.00005", "0.0"])#"0.25", "0.2", "0.15", "0.1", "0.05", "0.04", "0.03", "0.02", "0.01", "0.007", "0.005", "0.004", 
+Vs = np.array([-0.1])#"0.25", "0.2", "0.15", "0.1", "0.05", "0.04", "0.03", "0.02", "0.01", "0.007", "0.005", "0.004", 
 
 folder = "data/modes/square/dos_900/"
 element_names = ["a", "a+b", "a+ib"]
@@ -39,9 +39,9 @@ for T, U, V in iterate_containers(Ts, Us, Vs):
     name = f"T={T}/U={U}_V={V}"
     data, data_real, w_lin, res = cf.resolvent_data(f"{folder}{name}", name_suffix, 1e-6, plot_upper_lim, number_of_values=20000, imaginary_offset=1e-6)
     if realPart or both:
-        realPlotter.plot(w_lin, data_real, linewidth=(plt.rcParams["lines.linewidth"]), label=f"$V={V}$")
+        realPlotter.plot(w_lin, data_real, linewidth=(plt.rcParams["lines.linewidth"]), label=f"$T={T}$")
     if not realPart or both:
-        plotter.plot(w_lin, data, linewidth=(plt.rcParams["lines.linewidth"]), label=f"$V={V}$")
+        plotter.plot(w_lin, data, linewidth=(plt.rcParams["lines.linewidth"]), label=f"$T={T}$")
 
 #res.mark_continuum(ax)
 #legend = plt.legend(loc=8)
