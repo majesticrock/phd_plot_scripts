e1bedc7a0d81a14136373c544325106289b57418
majesticrock <joshua.althueser@googlemail.com>
2023-10-25 08:52:16 +0200

    data

--- plot_gap.py
+++ plot_gap.py
@@ -9,7 +9,7 @@ if(len(sys.argv) > 1):
     data_folder = "data/" + sys.argv[1] + "/"
     name = sys.argv[1]
 else:
-    name = "test"
+    name = "dos_T0"
     data_folder = f"data/phases/square/{name}/"
 
 x_axis_is_first = True
