628904a38ddc8079937f3bf34680c3d102d4ca1d
Joshua <joshua.althueser@gmail.com>
2023-11-17 10:28:50 +0100

    asd

--- lanczos_coeffs.py
+++ lanczos_coeffs.py
@@ -8,7 +8,7 @@ V = -0.1
 
 use_XP = True
 
-folder = "data/modes/cube/test/"
+folder = "data/modes/square/dos_1500/"
 name_suffix = "phase_SC"
 name = f"T={T}/U={U}/V={V}/"
 element_names = ["a", "a+b", "a+ib"]
