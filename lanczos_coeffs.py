eb6351411b8acce40bec4db7608f483d95fd5373
majesticrock <joshua.althueser@googlemail.com>
2023-11-21 16:04:53 +0100

    uni

--- lanczos_coeffs.py
+++ lanczos_coeffs.py
@@ -3,12 +3,12 @@ import matplotlib.pyplot as plt
 import gzip
 
 T = 0.
-U = -2.0
+U = -2.5
 V = -0.1
 
 use_XP = True
 
-folder = "data/modes/square/dos_2500/"
+folder = "data/modes/cube/dos_6k/"
 name_suffix = "phase_SC"
 name = f"T={T}/U={U}/V={V}/"
 element_names = ["a", "a+b", "a+ib"]
