d84215146afe890be07273e3fee81c55a788e7e1
Joshua <joshua.althueser@gmail.com>
2024-05-13 17:02:51 +0200

    ja to nevim

---
+++ continuum/lanczos_coeffs.py
@@ -0,0 +1,48 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import gzip
+
+T = 0.
+U = 2.5
+
+use_XP = True
+
+folder = "data/continuum/test/"
+name_suffix = "phase_SC"
+element_names = ["a", "a+b", "a+ib"]
+
+#file = f"{folder}{name}one_particle.dat.gz"
+#with gzip.open(file, 'rt') as f_open:
+#    one_particle = np.abs(np.loadtxt(f_open).flatten())
+#    roots = np.array([np.min(one_particle) * 2, np.max(one_particle) * 2])**2
+#    a_inf = (roots[0] + roots[1]) * 0.5
+#    b_inf = ((roots[1] - roots[0]) * 0.25)
+
+if use_XP:
+    file = f"{folder}resolvent_{name_suffix}.dat.gz"
+else:
+    element = element_names[0]
+    file = f"{folder}resolvent_{name_suffix}_{element}.dat.gz"
+with gzip.open(file, 'rt') as f_open:
+    M = np.loadtxt(f_open)
+    A = M[0]
+    B = M[1]
+
+#from scipy.signal import find_peaks
+#first_artifact = find_peaks(B[1:], prominence=5e2, width=1)
+#print(first_artifact)
+
+fig, ax = plt.subplots()
+ax.plot(A, ls="-", marker='x', label="$a_i$")
+ax.plot(np.sqrt(B), ls="-", marker='o', label="$b_i$")
+#ax.axhline(a_inf, linestyle="-" , color="k", label="$a_\\infty$")
+#ax.axhline(b_inf, linestyle="--", color="k", label="$b_\\infty$")
+ax.legend()
+ax.set_xlabel("Iteration $i$")
+ax.set_ylabel("Lanczos coefficient")
+#ax.set_ylim(13, 34)
+fig.tight_layout()
+
+import os
+plt.savefig(f"python/build/{os.path.basename(__file__).split('.')[0]}.pdf")
+plt.show()
---
+++ continuum/resolvent.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+import lib.continued_fraction as cf
+from lib.iterate_containers import naming_scheme
+import lib.plot_settings as ps
+# Calculates the resolvent in w^2
+
+prop_cycle = plt.rcParams['axes.prop_cycle']
+colors = prop_cycle.by_key()['color']
+
+Ts = np.array([0.])
+Us = np.array([-2.5])
+Vs = np.array([-0.1])
+
+use_XP = True
+
+folder = "data/continuum/test"
+fig, ax = plt.subplots()
+
+#ax.set_xscale("log")
+#ax.set_yscale("symlog")
+ax.set_ylim(-0.05, 1.)
+
+plotter = ps.CURVEFAMILY(6, axis=ax)
+plotter.set_individual_colors("nice")
+plotter.set_individual_linestyles(["-", "-.", "--", "-", "--", ":"])
+#plotter.set_individual_dashes()
+
+plot_lower_lim = -0.05
+plot_upper_lim = 10000
+
+name_suffix = "phase_SC"
+for name in naming_scheme(Ts, Us, Vs):
+    data, data_real, w_lin, res = cf.resolvent_data(f"{folder}", name_suffix, plot_lower_lim, plot_upper_lim, 
+                                                    number_of_values=20000, xp_basis=use_XP, imaginary_offset=1e-6, ingore_first=5, withTerminator=False)
+    plotter.plot(w_lin, data, label="Phase")
+
+name_suffix = "higgs_SC"
+for name in naming_scheme(Ts, Us, Vs):
+    data, data_real, w_lin, res = cf.resolvent_data(f"{folder}", name_suffix, plot_lower_lim, plot_upper_lim, 
+                                                    number_of_values=20000, xp_basis=use_XP, imaginary_offset=1e-6, ingore_first=5, withTerminator=False)
+    plotter.plot(w_lin, data, label="Higgs")
+
+res.mark_continuum(ax)
+legend = plt.legend()
+ax.add_artist(legend)
+
+ax.set_xlim(plot_lower_lim, plot_upper_lim)
+ax.set_xlabel(r"$\omega [t]$")
+ax.set_ylabel(r"$\mathcal{A} (\omega) [t^{-1}]$")
+fig.tight_layout()
+
+import os
+plt.savefig(f"python/build/{os.path.basename(__file__).split('.')[0]}.pdf")
+plt.show()
