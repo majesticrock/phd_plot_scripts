957a7c3432ae96c7d33780478934dbe7af245318
majesticrock <joshua.althueser@googlemail.com>
2024-05-31 16:21:46 +0200

    uni

--- continuum/expecs.py
+++ continuum/expecs.py
@@ -20,8 +20,14 @@ with gzip.open(f"data/continuum/approx_theta/expecs.dat.gz", 'rt') as f_open:
 ax.plot(M[0], M[1], ls="--", color="C0", linewidth=4)
 ax.plot(M[0], M[2], ls="--", color="C1", linewidth=4)
 
+with gzip.open(f"data/continuum/test/expecs.dat.gz", 'rt') as f_open:
+    M = np.loadtxt(f_open)
+    
+ax.plot(M[0], M[1], ls=":", color="C0", linewidth=4)
+ax.plot(M[0], M[2], ls=":", color="C1", linewidth=4)
+
 color_and_linestyle_legends(ax, color_labels=[r"$\langle n_k \rangle$", r"$\langle f_k \rangle$"], 
-                            linestyle_labels=["Exact interaction", "Approx. interaction"])
+                            linestyle_labels=["Exact interaction", "Approx. interaction", "With Coulomb"])
 
 ax.set_xlabel("$k$")
 ax.set_ylabel("$<O>$")
--- continuum/gap_test.py
+++ continuum/gap_test.py
@@ -8,12 +8,18 @@ fig, ax = plt.subplots()
 
 with gzip.open(f"data/continuum/test/gap.dat.gz", 'rt') as f_open:
     M = np.loadtxt(f_open)
-ax.plot(M[0], MEV_FACTOR * M[1], "-", label=r"SC")
-ax.plot(M[0], MEV_FACTOR * M[2], "-", label=r"OCC")
+
+delta_abs = np.sqrt(M[1]**2 + M[2]**2)
+delta_phase = np.arctan2(M[2], M[1])
+ax.plot(M[0], MEV_FACTOR * delta_abs, "-", label=r"Abs SC")
+
+ax2 = ax.twinx()
+ax2.plot(M[0], delta_phase / np.pi, "--", color="C3", label=r"Phase SC")
+ax2.set_ylabel(r"arg[$\Delta$] / $\pi$")
 
 ax.set_xlabel(r"$k [\sqrt{\mathrm{eV}}]$")
 ax.set_ylabel(r"$\Delta [\mathrm{meV}]$")
-ax.legend()
+fig.legend()
 fig.tight_layout()
 
 plt.show()
