cc4fe01a0bb676c8da0c2a799db22c192276075a
majesticrock <joshua.althueser@googlemail.com>
2024-06-18 15:52:18 +0200

    uni

--- continuum/expecs.py
+++ continuum/expecs.py
@@ -10,18 +10,21 @@ fig, ax = plt.subplots()
 
 with gzip.open(f"data/continuum/exact_theta/expecs.dat.gz", 'rt') as f_open:
     M = np.loadtxt(f_open)
+    M[0] = M[0] - M[0][int(len(M[0]) / 2)]
     
 ax.plot(M[0], M[1], ls="-", color="C0")
 ax.plot(M[0], M[2], ls="-", color="C1")
 
 with gzip.open(f"data/continuum/approx_theta/expecs.dat.gz", 'rt') as f_open:
     M = np.loadtxt(f_open)
+    M[0] = M[0] - M[0][int(len(M[0]) / 2)]
     
 ax.plot(M[0], M[1], ls="--", color="C0", linewidth=4)
 ax.plot(M[0], M[2], ls="--", color="C1", linewidth=4)
 
 with gzip.open(f"data/continuum/test/expecs.dat.gz", 'rt') as f_open:
     M = np.loadtxt(f_open)
+    M[0] = M[0] - M[0][int(len(M[0]) / 2)]
     
 ax.plot(M[0], M[1], ls=":", color="C0", linewidth=4)
 ax.plot(M[0], M[2], ls=":", color="C1", linewidth=4)
@@ -29,7 +32,7 @@ ax.plot(M[0], M[2], ls=":", color="C1", linewidth=4)
 color_and_linestyle_legends(ax, color_labels=[r"$\langle n_k \rangle$", r"$\langle f_k \rangle$"], 
                             linestyle_labels=["Exact interaction", "Approx. interaction", "With Coulomb"])
 
-ax.set_xlabel("$k$")
+ax.set_xlabel(r"$k - k_\mathrm{F} [\sqrt{\mathrm{meV}}]$")
 ax.set_ylabel("$<O>$")
 
 fig.tight_layout()
--- continuum/gap_test.py
+++ continuum/gap_test.py
@@ -3,22 +3,39 @@ import numpy as np
 import gzip
 
 MEV_FACTOR = 1e3
+plotPhase = False
 
 fig, ax = plt.subplots()
 
 with gzip.open(f"data/continuum/test/gap.dat.gz", 'rt') as f_open:
     M = np.loadtxt(f_open)
+M[0] = M[0] - M[0][int(0.5 * len(M[0])) - 1]
 
-delta_abs = np.sqrt(M[1]**2 + M[2]**2)
-delta_phase = np.arctan2(M[2], M[1])
-ax.plot(M[0], MEV_FACTOR * delta_abs, "-", label=r"Abs SC")
-ax.plot(M[0], MEV_FACTOR * M[3], "-", label=r"$\delta n$")
+if plotPhase:
+    delta_abs = np.sqrt(M[1]**2 + M[2]**2)
+    delta_phase = np.arctan2(M[2], M[1])
+    M[1] = delta_abs
+    ax.plot(M[0], MEV_FACTOR * delta_abs, "-", label=r"Abs SC")
+    ax2 = ax.twinx()
+    ax2.plot(M[0], delta_phase / np.pi, "--", color="C3", label=r"Phase SC")
+    ax2.set_ylabel(r"arg[$\Delta$] / $\pi$")
+else:
+    ax.plot(M[0], MEV_FACTOR * M[1], "-", label=r"$\Delta_\mathrm{SC}$")
+ax.plot(M[0], MEV_FACTOR * M[3], "-", label=r"$\Delta_n$")
 
-ax2 = ax.twinx()
-ax2.plot(M[0], delta_phase / np.pi, "--", color="C3", label=r"Phase SC")
-ax2.set_ylabel(r"arg[$\Delta$] / $\pi$")
+from scipy.optimize import curve_fit
+use_points = .6
+fit_data_x = M[0][int(use_points * len(M[0])):]
+fit_data_y = MEV_FACTOR * M[1][int(use_points * len(M[1])):]
 
-ax.set_xlabel(r"$k [\sqrt{\mathrm{eV}}]$")
+def func(x, a, b):
+    return a / x**b
+popt, pcov = curve_fit(func, fit_data_x, fit_data_y)
+print(popt)
+ax.plot(fit_data_x, func(fit_data_x, *popt), ls="--", color="C3", label="Fit")
+
+
+ax.set_xlabel(r"$k - k_\mathrm{F} [\sqrt{\mathrm{eV}}]$")
 ax.set_ylabel(r"$\Delta [\mathrm{meV}]$")
 fig.legend()
 fig.tight_layout()
