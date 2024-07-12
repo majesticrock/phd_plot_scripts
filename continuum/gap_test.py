2da05f6a31fec08ab6d1ce762915b6b33764a719
majesticrock <joshua.althueser@googlemail.com>
2024-07-12 10:14:21 +0200

    Save uni

--- continuum/gap_test.py
+++ continuum/gap_test.py
@@ -1,4 +1,3 @@
-from turtle import pd
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -7,11 +6,11 @@ __ap.append()
 from create_zoom import create_zoom
 from get_data import *
 
-MEV_FACTOR = 1e3
+X_BOUNDS = [-0.1, 0.1]
 
 fig, ax = plt.subplots()
 
-main_df = load_panda("continuum", "approx_2000", "gap.json.gz", 
+main_df = load_panda("continuum", "exact_2000", "gap.json.gz", 
                      **continuum_params(0.0, 1., 9.3, 10., 10.))
 pd_data = main_df["data"]
 pd_data["ks"] -= main_df["k_F"]
@@ -19,10 +18,9 @@ pd_data["ks"] -= main_df["k_F"]
 ax.plot(pd_data["ks"], pd_data["Delta_Phonon"] + pd_data["Delta_Coulomb"], "k-", label=r"$\Delta_\mathrm{SC}$")
 ax.plot(pd_data["ks"], pd_data["Delta_Phonon"], "--", label=r"$\Delta_\mathrm{Phonon}$")
 ax.plot(pd_data["ks"], pd_data["Delta_Coulomb"], "--", label=r"$\Delta_\mathrm{Coulomb}$")
-#ax.plot(pd_data["ks"], pd_data["Delta_cut"], ":", label=r"$\Delta_\mathrm{cut}$")
 ax.plot(pd_data["ks"], pd_data["Delta_Fock"], "-", label=r"$\Delta_\mathrm{Fock}$")
 
-#axins = create_zoom(ax, 0.4, 0.3, 0.3, 0.6, xlim=(-0.02, 0.02), ylim=(1.1 * np.min(pd_data["Delta_Coulomb"]), 1.05 * np.max(pd_data["Delta_Phonon"])))
+axins = create_zoom(ax, 0.4, 0.3, 0.3, 0.6, xlim=(-0.02, 0.02), ylim=(1.1 * np.min(pd_data["Delta_Coulomb"]), 1.05 * np.max(pd_data["Delta_Phonon"])))
 
 ax.set_xlabel(r"$k - k_\mathrm{F} [\sqrt{\mathrm{eV}}]$")
 ax.set_ylabel(r"$\Delta [\mathrm{meV}]$")
--- continuum/gap_theta.py
+++ continuum/gap_theta.py
@@ -1,29 +1,34 @@
 import matplotlib.pyplot as plt
 import numpy as np
-import gzip
 
-MEV_FACTOR = 1e3
+import __path_appender as __ap
+__ap.append()
+from get_data import *
 
-def energy(k, delta):
-    return np.sqrt((0.5 * k**2 - 9.3)**2 + np.abs(delta)**2)
+X_BOUNDS = [-0.05, 0.05]
 
 fig, ax = plt.subplots()
 
-with gzip.open(f"data/continuum/exact_theta/gap.dat.gz", 'rt') as f_open:
-    M = np.loadtxt(f_open)
-ax.plot(M[0], MEV_FACTOR * M[1], "-", label=r"$\theta(\omega_\mathrm{D} - |\epsilon_k - \epsilon_l|)$")
+main_df = load_panda("continuum", "exact_2000", "gap.json.gz", **continuum_params(0.0, 1., 9.3, 10., 10.))
+pd_data = main_df["data"]
+pd_data["ks"] -= main_df["k_F"]
+plot_range = pd_data.query(f'ks > {1.2 * X_BOUNDS[0]} & ks < {1.2 * X_BOUNDS[1]}')
+ax.plot(plot_range["ks"], plot_range["Delta_Phonon"] + plot_range["Delta_Coulomb"], "-", c="C0", label=r"$\Delta_\mathrm{SC}$")
+ax.plot(plot_range["ks"], plot_range["Delta_Phonon"], "-.", c="C0", label=r"$\Delta_\mathrm{Phonon}$")
+ax.plot(plot_range["ks"], plot_range["Delta_Coulomb"], "--", c="C0", label=r"$\Delta_\mathrm{Coulomb}$")
 
-ax2 = ax.twinx()
-ax2.plot(M[0], energy(M[0], M[1]), "--")
+main_df = load_panda("continuum", "approx_2000", "gap.json.gz", **continuum_params(0.0, 1., 9.3, 10., 10.))
+pd_data = main_df["data"]
+pd_data["ks"] -= main_df["k_F"]
+plot_range = pd_data.query(f'ks > {1.2 * X_BOUNDS[0]} & ks < {1.2 * X_BOUNDS[1]}')
+ax.plot(plot_range["ks"], plot_range["Delta_Phonon"] + plot_range["Delta_Coulomb"], "-", c="C1", label=r"$\Delta_\mathrm{SC}$")
+ax.plot(plot_range["ks"], plot_range["Delta_Phonon"], "-.", c="C1", label=r"$\Delta_\mathrm{Phonon}$")
+ax.plot(plot_range["ks"], plot_range["Delta_Coulomb"], "--", c="C1", label=r"$\Delta_\mathrm{Coulomb}$")
 
-with gzip.open(f"data/continuum/approx_theta/gap.dat.gz", 'rt') as f_open:
-    M_approx = np.loadtxt(f_open)
-ax.plot(M_approx[0], MEV_FACTOR * M_approx[1], "-", label=r"$\theta(\omega_\mathrm{D} - \epsilon_k) \theta(\omega_\mathrm{D} - \epsilon_l)$")
-ax2.plot(M_approx[0], energy(M_approx[0], M_approx[1]), "--")
-
-ax.set_xlabel(r"$k [\sqrt{\mathrm{eV}}]$")
+ax.set_xlim(*X_BOUNDS)
+ax.set_xlabel(r"$k - k_\mathrm{F} [\sqrt{\mathrm{eV}}]$")
 ax.set_ylabel(r"$\Delta [\mathrm{meV}]$")
-ax2.set_ylabel(r"$E(k) [\mathrm{eV}]$")
+
 ax.legend()
 fig.tight_layout()
 
