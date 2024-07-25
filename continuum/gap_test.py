eddfad5329a87422561d3dfcd7eb42de9b1fb7e8
majesticrock <joshua.althueser@googlemail.com>
2024-07-25 09:13:40 +0200

    backup

--- continuum/gap_test.py
+++ continuum/gap_test.py
@@ -10,15 +10,19 @@ X_BOUNDS = [-0.1, 0.1]
 
 fig, ax = plt.subplots()
 
-main_df = load_panda("continuum", "mielke_2000", "gap.json.gz", 
-                     **continuum_params(0.0, 1., 9.3, 10., 10.))
+main_df = load_panda("continuum", "test", "gap.json.gz", 
+                     **continuum_params(0.0, 0.0, 9.3, 10., 10.))
 pd_data = main_df["data"]
-pd_data["ks"] -= main_df["k_F"]
+pd_data["ks"] /= main_df["k_F"]
 if pd_data["Delta_Coulomb"][0] > 0:
     pd_data["Delta_Phonon"] *= -1
     pd_data["Delta_Coulomb"] *= -1
 ax.plot(pd_data["ks"], pd_data["Delta_Phonon"] + pd_data["Delta_Coulomb"], "k-", label=r"$\Delta_\mathrm{SC}$")
 pd_data.plot(x="ks", y=["Delta_Phonon", "Delta_Coulomb", "Delta_Fock"], ax=ax, style=['--', '--', '-'], label=[r"$\Delta_\mathrm{Phonon}$", r"$\Delta_\mathrm{Coulomb}$", r"$\Delta_\mathrm{Fock}$"])
+
+inner = int((main_df["discretization"] - main_df["inner_discretization"]) / 2)
+ax.axvline(pd_data["ks"][inner], ls="-", color="k")
+ax.axvline(pd_data["ks"][inner + main_df["inner_discretization"]], ls="-", color="k")
 #(pd_data["Delta_Phonon"] + pd_data["Delta_Coulomb"]).plot(ax=ax, x="ks", style="k-", label=r"$\Delta_\mathrm{SC}$")
 
 #from scipy import integrate
@@ -26,7 +30,7 @@ pd_data.plot(x="ks", y=["Delta_Phonon", "Delta_Coulomb", "Delta_Fock"], ax=ax, s
 #ax.text(0.4, 0.05, fr'$\int \Delta_\mathrm{{Coulomb}} \mathrm{{d}}k / k_\mathrm{{F}} = {integrate.trapezoid(pd_data["Delta_Coulomb"], x=pd_data["ks"]) / main_df["k_F"]:.5f}$ meV', transform=ax.transAxes)
 #axins = create_zoom(ax, 0.4, 0.3, 0.3, 0.6, xlim=(-0.02, 0.02), ylim=(1.1 * np.min(pd_data["Delta_Coulomb"]), 1.05 * np.max(pd_data["Delta_Phonon"])))
 
-ax.set_xlabel(r"$k - k_\mathrm{F} [\sqrt{\mathrm{eV}}]$")
+ax.set_xlabel(r"$k / k_\mathrm{F}$")
 ax.set_ylabel(r"$\Delta [\mathrm{meV}]$")
 
 ax.legend()
