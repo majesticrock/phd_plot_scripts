07c3203e0f0e394370261d5a7e7d9d27e5c19973
Joshua <joshua.althueser@gmail.com>
2024-08-23 15:49:08 +0200

    home

--- continuum/resolvent.py
+++ continuum/resolvent.py
@@ -4,10 +4,10 @@ import __path_appender as __ap
 __ap.append()
 
 from get_data import load_panda, continuum_params
-pd_data = load_panda("continuum", "offset_10", "resolvents.json.gz", **continuum_params(12000, 0., 1., 1e-4, 4.25, 5., 10.))
+pd_data = load_panda("continuum", "offset_10", "resolvents.json.gz", **continuum_params(12000, 0., 1., 1e-4, 4.25, 4., 10.))
 
 import continued_fraction_pandas as cf
-import plot_settings as ps
+import plot_settings as ps 
 
 resolvents = cf.ContinuedFraction(pd_data, ignore_first=5, ignore_last=90)
 
@@ -21,8 +21,8 @@ plotter.set_individual_colors("nice")
 plotter.set_individual_linestyles(["-", "-.", "--", "-", "--", ":"])
 
 w_lin = np.linspace(-0.01 * pd_data["continuum_boundaries"][0], 1.1 * pd_data["continuum_boundaries"][1], 15000, dtype=complex)
-#w_lin = np.linspace(10, 100, 15000, dtype=complex)
-w_lin += 1e-4j
+#w_lin = np.linspace(0, 5, 15000, dtype=complex)
+w_lin += 1e-5j
 
 plotter.plot(1e3 * w_lin.real, resolvents.spectral_density(w_lin, "phase_SC",     withTerminator=True), label="Phase")
 plotter.plot(1e3 * w_lin.real, resolvents.spectral_density(w_lin, "amplitude_SC", withTerminator=True), label="Higgs")
