6de1a19a755165f314a98b25bbb394622ed5a80c
majesticrock <joshua.althueser@googlemail.com>
2024-11-19 08:45:54 +0100

    pathing

--- continuum/__path_appender.py
+++ continuum/__path_appender.py
@@ -1,8 +1,9 @@
 import os, sys
 
 def __base():
-    return os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))) # parent folder of "phd"
+    return os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #  "phd"
 
 def append():
     sys.path.append(os.path.join(__base(), "PhdUtility", "python"))
     sys.path.append(os.path.join(__base(), "raw_data_phd"))
+    sys.path.append(os.path.join(__base(), "data"))
