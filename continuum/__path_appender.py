2caad4a7e68403515a3ea2aee7199354ae065d79
majesticrock <joshua.althueser@googlemail.com>
2025-01-06 09:35:52 +0100

    successful on workstation

--- continuum/__path_appender.py
+++ continuum/__path_appender.py
@@ -4,7 +4,7 @@ def __base():
     return os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) #  "phd"
 
 def append():
-    sys.path.append(os.path.join(__base(), "Phdutility", "python"))
+    sys.path.append(os.path.join(__base(), "PhdUtility", "python"))
     sys.path.append(os.path.join(os.path.dirname(__base()), "raw_data_phd"))
     sys.path.append(os.path.join(__base(), "data"))
     
--- tests/__path_appender.py
+++ tests/__path_appender.py
@@ -4,6 +4,7 @@ def __base():
     return os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))) # "phd"
 
 def append():
-    sys.path.append(os.path.join(__base(), "Phdutility", "python"))
+    print(__base())
+    sys.path.append(os.path.join(__base(), "PhdUtility", "python"))
     sys.path.append(os.path.join(os.path.dirname(__base()), "raw_data_phd"))
     sys.path.append(os.path.join(__base(), "data"))
