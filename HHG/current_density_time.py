90181f5874a0135b8f1a33fc7ba742c6e80baa34
majesticrock <joshua.althueser@googlemail.com>
2025-04-11 16:06:17 +0200

    GUI

--- HHG/current_density_time.py
+++ HHG/current_density_time.py
@@ -6,40 +6,49 @@ __path_appender.append()
 from get_data import *
 from legend import *
 
-def plot_data(df, label, **kwargs):
-    times = np.linspace(main_df["t_begin"], main_df["t_end"], len(main_df["current_density_time"])) / (2 * np.pi)
-    ax.plot(times, main_df["current_density_time"], label=label, **kwargs)
+def plot_data(df, ax, label, **kwargs):
+    times = np.linspace(df["t_begin"], df["t_end"], len(df["current_density_time"])) / (2 * np.pi)
+    ax.plot(times, df["current_density_time"], label=label, **kwargs)
 
-fig, ax = plt.subplots()
+def plot_j(df):
+    fig, ax = plt.subplots()
+    plot_data(df, ax, r"No decay $W=100$")
+    ax.set_xlabel(legend(r"t / T_\mathrm{L}")) # T_L = 2 pi / omega_L
+    ax.set_ylabel(legend(r"j(t)"))
+    ax.legend()
 
-#main_df = load_panda("HHG", "decay/cosine_laser", "current_density.json.gz", 
-#                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=30))
-#plot_data(main_df, r"$W=20, \tau=30$")
-#
-#main_df = load_panda("HHG", "decay/cosine_laser", "current_density.json.gz", 
-#                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=40, field_amplitude=1.6, photon_energy=5.25, decay_time=30))
-#plot_data(main_df, r"$W=40, \tau=30$", ls="--")
-#
-#main_df = load_panda("HHG", "decay/cosine_laser", "current_density.json.gz", 
-#                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=40, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
-#plot_data(main_df, r"$W=40, \tau=10$", ls="-.")
-##########
-#main_df = load_panda("HHG", "decay_4_cycle/cosine_laser", "current_density.json.gz", 
-#                     **hhg_params(T=0, E_F=0, v_F=1.5e5, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
-#plot_data(main_df, r"2 cycles", ls="-")
-#main_df = load_panda("HHG", "test_4_cycle/cosine_laser", "current_density.json.gz", 
-#                     **hhg_params(T=0, E_F=0, v_F=1.5e5, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
-#plot_data(main_df, r"2 cycles", ls="-")
-##########
+    fig.tight_layout()
+    plt.show()
 
-main_df = load_panda("HHG", "2_cycle/cosine_laser", "current_density.json.gz", 
-                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
-plot_data(main_df, r"No decay $W=100$")
+if __name__ == '__main__':
+    fig, ax = plt.subplots()
+    ax.set_xlabel(legend(r"t / T_\mathrm{L}")) # T_L = 2 pi / omega_L
+    ax.set_ylabel(legend(r"j(t)"))
+    ax.legend()
 
-ax.set_xlabel(legend(r"t / T_\mathrm{L}")) # T_L = 2 pi / omega_L
-ax.set_ylabel(legend(r"j(t)"))
+    fig.tight_layout()
+    
+    #main_df = load_panda("HHG", "decay/cosine_laser", "current_density.json.gz", 
+    #                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=30))
+    #plot_data(main_df, ax, r"$W=20, \tau=30$")
+    #
+    #main_df = load_panda("HHG", "decay/cosine_laser", "current_density.json.gz", 
+    #                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=40, field_amplitude=1.6, photon_energy=5.25, decay_time=30))
+    #plot_data(main_df, ax, r"$W=40, \tau=30$", ls="--")
+    #
+    #main_df = load_panda("HHG", "decay/cosine_laser", "current_density.json.gz", 
+    #                     **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=40, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
+    #plot_data(main_df, ax, r"$W=40, \tau=10$", ls="-.")
+    ##########
+    #main_df = load_panda("HHG", "decay_4_cycle/cosine_laser", "current_density.json.gz", 
+    #                     **hhg_params(T=0, E_F=0, v_F=1.5e5, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
+    #plot_data(main_df, ax, r"2 cycles", ls="-")
+    #main_df = load_panda("HHG", "test_4_cycle/cosine_laser", "current_density.json.gz", 
+    #                     **hhg_params(T=0, E_F=0, v_F=1.5e5, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
+    #plot_data(main_df, ax, r"2 cycles", ls="-")
+    ##########
 
-ax.legend()
-
-fig.tight_layout()
-plt.show()
+    main_df = load_panda("HHG", "2_cycle/cosine_laser", "current_density.json.gz", 
+                         **hhg_params(T=0, E_F=0, v_F=1.5e4, band_width=20, field_amplitude=1.6, photon_energy=5.25, decay_time=10))
+    plot_data(main_df, ax, r"No decay $W=100$")
+    plt.show()
--- HHG/gui.py
+++ HHG/gui.py
@@ -3,28 +3,29 @@ import tkinter as tk
 from tkinter import filedialog, ttk
 from collections import defaultdict
 
+import pandas as pd
+
 import __path_appender
 __path_appender.append()
 from get_data import *
-from current_density_fourier import plot_j
+import current_density_fourier
+import current_density_time
 
 class ParamSelector(tk.Tk):
     def __init__(self):
         super().__init__()
         self.title("Parameter Selector")
 
-        self.param_tree = defaultdict(set)
-        self.valid_paths = []
-
+        self.df_paths = pd.DataFrame()
         self.param_order = []
-        self.current_selection = {}
         self.dropdowns = {}
         self.dd_vars = {}
 
         # Optional dialog, only if you want it
-        ask_user = True  # flip this to True to show dialog
+        ask_user = False  # flip this to True to show dialog
         default_path = os.path.abspath("data/HHG/4_cycle/cosine_laser")
-        print(default_path)
+        self.name_type = "4_cycle/cosine_laser"
+
         if ask_user:
             self.base_dir = filedialog.askdirectory(
                 title="Select base directory", initialdir=default_path
@@ -40,12 +41,39 @@ class ParamSelector(tk.Tk):
 
         self.parse_directory()
         self.create_dropdowns()
+        self.create_menu_buttons()
 
-        # Add "Load Data" button
-        self.load_button = tk.Button(self, text="Load Data", command=self.load_data)
-        self.load_button.grid(row=len(self.param_order), column=0, columnspan=2, pady=10)
+    def create_menu_buttons(self):
+        row = len(self.param_order) + 1
+
+        self.change_btn = tk.Button(self, text="Change Directory", command=self.change_directory)
+        self.change_btn.grid(row=row, column=0, columnspan=2, pady=5)
+
+        self.j_time_button = tk.Button(self, text="Plot j(t)", command=self.j_time)
+        self.j_time_button.grid(row=row + 1, column=0, pady=5, padx=5)
+        
+        self.fft_button = tk.Button(self, text="Plot j(w)", command=self.fft)
+        self.fft_button.grid(row=row + 1, column=1, pady=5, padx=5)
+
+    def change_directory(self):
+        new_dir = filedialog.askdirectory(title="Select new base directory", initialdir=self.base_dir)
+        if new_dir:
+            self.base_dir = new_dir
+            
+            path_parts = os.path.normpath(new_dir).split(os.sep)
+            if len(path_parts) >= 2:
+                self.name_type = os.path.join(path_parts[-2], path_parts[-1])
+                print("Selected:", self.name_type)
+            else:
+                self.name_type = ""
+                print("Warning: Could not extract name/type")
+            
+            self.parse_directory()
+            self.create_dropdowns()
+            self.create_menu_buttons()
 
     def parse_directory(self):
+        records = []
         for root, dirs, files in os.walk(self.base_dir):
             if any(f.endswith('.json.gz') for f in files):
                 rel_path = os.path.relpath(root, self.base_dir)
@@ -55,46 +83,76 @@ class ParamSelector(tk.Tk):
                     if '=' in part:
                         key, value = part.split('=')
                         param_dict[key] = value
-                self.valid_paths.append(param_dict)
+                records.append(param_dict)
 
-        all_params = set()
-        for path in self.valid_paths:
-            all_params.update(path.keys())
-        self.param_order = sorted(all_params)
+        self.df_paths = pd.DataFrame(records)
+        self.df_paths.dropna(how='any', inplace=True)
+        self.param_order = sorted(self.df_paths.columns.tolist())
 
     def create_dropdowns(self):
+        def make_callback(p, v):
+            previous = [""]  # use a mutable container
+            def callback(*args):
+                old_value = previous[0]
+                previous[0] = v.get()
+                self.update_options(args[0], old_value, previous[0])
+
+            return callback
+        
         for i, param in enumerate(self.param_order):
             label = tk.Label(self, text=param)
             label.grid(row=i, column=0, padx=5, pady=5)
 
-            self.dd_vars[param] = tk.StringVar(self)
-            #self.dd_vars[param].trace_add("write", lambda *_: self.update_options())
+            self.dd_vars[param] = tk.StringVar(self, name=param)        
+            self.dd_vars[param].trace_add("write", make_callback(param, self.dd_vars[param]))
 
             dropdown = ttk.Combobox(self, textvariable=self.dd_vars[param], state="readonly")
             dropdown.grid(row=i, column=1, padx=5, pady=5)
 
             self.dropdowns[param] = dropdown
-            self.current_selection[param] = None
 
         self.update_options()
         #for param in self.param_order:
         #    self.dropdowns[param].current(0)
 
-    def update_options(self):
-        selection = {param: self.dropdowns[param].get() for param in self.param_order if self.dropdowns[param].get()}
+    def create_query(self):
+        selected_values = {param: self.dropdowns[param].get() for param in self.param_order if self.dropdowns[param].get()}
+        query_parts = [f"{param}=='{value}'" for param, value in selected_values.items()]
+        query_string = " & ".join(query_parts)
+        return query_string
 
-        filtered_paths = self.valid_paths
+    def update_options(self, changed_param=None, changed_from='', changed_to=''):
+        if changed_from != '' and changed_to != '':
+            selection = {param: self.dd_vars[param].get() for param in self.param_order if self.dd_vars[param].get()}
+            df_filtered = self.df_paths.copy()
+            for param, val in selection.items():
+                df_filtered = df_filtered[df_filtered[param] == val]
+            if len(df_filtered) == 0: 
+                for param in self.param_order:
+                    if param == changed_param:
+                        continue
+                    self.dropdowns[param].set('')      
+        
+        # Build selection from dropdowns
+        selection = {param: self.dd_vars[param].get() for param in self.param_order if self.dd_vars[param].get()}
+    
+        # Filter paths according to current valid selection
+        df_filtered = self.df_paths.copy()
         for param, val in selection.items():
-            filtered_paths = [p for p in filtered_paths if p.get(param) == val]
-
+            df_filtered = df_filtered[df_filtered[param] == val]
+    
+        # Update each dropdown's value list
         for param in self.param_order:
-            possible_values = sorted({p[param] for p in filtered_paths if param in p})
-            current = self.dropdowns[param].get()
-            self.dropdowns[param]['values'] = possible_values
-            if current not in possible_values:
-                self.dropdowns[param].set('')
+            all_values = sorted(self.df_paths[param].dropna().unique())
+            valid_values = sorted(df_filtered[param].dropna().unique())
+    
+            current_val = self.dd_vars[param].get()
+            if current_val:
+                self.dropdowns[param]['values'] = all_values
+            else:
+                self.dropdowns[param]['values'] = valid_values
 
-    def load_data(self):
+    def fft(self):
         selected_values = {param: self.dropdowns[param].get() for param in self.param_order}
         if not all(selected_values.values()):
             print("Please make sure all parameters are selected.")
@@ -102,11 +160,24 @@ class ParamSelector(tk.Tk):
 
         # Call your function
         main_df = load_panda(
-            "HHG", "4_cycle/cosine_laser", "current_density.json.gz",
+            "HHG", self.name_type, "current_density.json.gz",
             **hhg_params(**selected_values)
         )
 
-        plot_j(main_df)
+        current_density_fourier.plot_j(main_df)
+        
+    def j_time(self):
+        selected_values = {param: self.dropdowns[param].get() for param in self.param_order}
+        if not all(selected_values.values()):
+            print("Please make sure all parameters are selected.")
+            return
+
+        # Call your function
+        main_df = load_panda(
+            "HHG", self.name_type, "current_density.json.gz",
+            **hhg_params(**selected_values)
+        )
+        current_density_time.plot_j(main_df)
 
 if __name__ == "__main__":
     app = ParamSelector()
