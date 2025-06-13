aa324135bce5e93f8d4b7f80112bf8880891269c
majesticrock <joshua.althueser@googlemail.com>
2025-06-13 16:28:09 +0200

    fft direct

--- HHG/experiment.py
+++ HHG/experiment.py
@@ -12,9 +12,9 @@ MODEL = "PiFlux"
 v_F = 1.5e5
 W = 400
 
-df_A = load_panda("HHG", f"test/expA_laser/{MODEL}", "current_density.json.gz", 
+df_A = load_panda("HHG", f"exp_base/expA_laser/{MODEL}", "current_density.json.gz", 
                     **hhg_params(T=300, E_F=118, v_F=v_F, band_width=W, field_amplitude=1., photon_energy=1., decay_time=100))
-df_B = load_panda("HHG", f"test/expB_laser/{MODEL}", "current_density.json.gz", 
+df_B = load_panda("HHG", f"exp_base/expB_laser/{MODEL}", "current_density.json.gz", 
                     **hhg_params(T=300, E_F=118, v_F=v_F, band_width=W, field_amplitude=1., photon_energy=1., decay_time=100))
 
 signal_A = cdf.compute_current_density(df_A)
@@ -22,9 +22,9 @@ signal_B = cdf.compute_current_density(df_B)
 
 def combined_signal(omega, t0):
     if t0 > 0:
-        return signal_A + signal_B * np.exp(1.0j * omega + t0)
+        return signal_A + signal_B * np.exp(1.0j * omega * t0)
     else:
-        return signal_A * np.exp(-1.0j * omega + t0) + signal_B
+        return signal_A * np.exp(-1.0j * omega * t0) + signal_B
 
 fig, axes = cdf.create_frame(nrows=3, figsize=(6.4, 8), 
                              ylabel_list=[legend(r"|\omega j_\mathrm{lin}(\omega) |", "a.b."), 
@@ -38,15 +38,16 @@ cdf.add_verticals(frequencies, axes[2], max_freq=40, positions='odd' if MODEL !=
 
 axes[0].set_xlim(0, 40)
 
-for i, t0 in enumerate([0, 0.5, 1, 2, 3, 4]):
-    main_df = load_panda("HHG", f"test_{t0}/exp_laser/{MODEL}", "current_density.json.gz", 
+for i, t0 in enumerate([0, 0.5, 1, 2, 3, 4, 6]):
+    main_df = load_panda("HHG", f"exp_{t0}/exp_laser/{MODEL}", "current_density.json.gz", 
                         **hhg_params(T=300, E_F=118, v_F=v_F, band_width=W, field_amplitude=1., photon_energy=1., decay_time=100))
     
     t0_unitless = t0 * main_df["photon_energy"] / (0.6582119569509065) # the number is hbar in meV * ps
     frequencies2 = main_df["frequencies"]
-    axes[0].plot(frequencies, np.abs(combined_signal(frequencies, t0_unitless)), label=f"$t_0 = {t0}$ ps")
-    cdf.add_current_density_to_plot(main_df, axes[1], max_freq=40, label=f"Proper $t_0 = {t0}$ ps", shift=10**(-i), normalize=True)
-    cdf.add_current_density_to_plot(main_df, axes[2], max_freq=40, substract=combined_signal(frequencies, t0_unitless), label=f"NL $t_0 = {t0}$ ps", shift=10**(-i), normalize=True)
+    plt_signal = 2 * np.abs(combined_signal(frequencies, t0_unitless))
+    axes[0].plot(frequencies, 10**(-i) * plt_signal, label=f"$t_0 = {t0}$ ps")
+    cdf.add_current_density_to_plot(main_df, axes[1], max_freq=40, label=f"Proper $t_0 = {t0}$ ps", normalize=False, shift=10**(-i))
+    cdf.add_current_density_to_plot(main_df, axes[2], max_freq=40, substract=combined_signal(frequencies, t0_unitless), label=f"NL $t_0 = {t0}$ ps", normalize=False, shift=10**(-i))
 
 axes[0].legend(loc='upper right')
 
--- HHG/experiment_time.py
+++ HHG/experiment_time.py
@@ -9,13 +9,15 @@ __path_appender.append()
 from get_data import *
 from legend import *
 
+from scipy.fft import rfft, rfftfreq
+
 MODEL = "PiFlux"
-v_F = 1.5e5
+v_F = 1.5e6
 W = 400
 
-df_A = load_panda("HHG", f"test/expA_laser/{MODEL}", "current_density.json.gz", 
+df_A = load_panda("HHG", f"exp_base/expA_laser/{MODEL}", "current_density.json.gz", 
                     **hhg_params(T=300, E_F=118, v_F=v_F, band_width=W, field_amplitude=1., photon_energy=1., decay_time=100))
-df_B = load_panda("HHG", f"test/expB_laser/{MODEL}", "current_density.json.gz", 
+df_B = load_panda("HHG", f"exp_base/expB_laser/{MODEL}", "current_density.json.gz", 
                     **hhg_params(T=300, E_F=118, v_F=v_F, band_width=W, field_amplitude=1., photon_energy=1., decay_time=100))
 
 times = np.linspace(0, df_A["t_end"] - df_A["t_begin"], len(df_A["current_density_time"])) / (2 * np.pi)
@@ -41,6 +43,10 @@ fig, axes = cdt.create_frame(nrows=3, figsize=(6.4, 8),
                                           legend(r"j_\mathrm{sim}(t)", "a.b."), 
                                           legend(r"j_\mathrm{sim}(t) - j_\mathrm{lin}(t)", "a.b.")])
 #fig_laser, ax_laser = plt.subplots()
+fig_fft, axes_fft = plt.subplots(nrows=3, figsize=(6.4, 8), sharex=True)
+for ax in axes_fft:
+    ax.set_yscale('log')
+    ax.set_xlim(0, 40)
 
 inter_laser_A = interp1d(times, df_A["laser_function"], fill_value=0.0, bounds_error=False)
 inter_laser_B = interp1d(times, df_B["laser_function"], fill_value=0.0, bounds_error=False)
@@ -53,15 +59,27 @@ def combined_laser(t, t0):
         B = inter_laser_B(t)
     return A + B
 
-for i, t0 in enumerate([0, 0.5, 1, 2, 3, 4]):
-    main_df = load_panda("HHG", f"test_{t0}/exp_laser/{MODEL}", "current_density.json.gz", 
+for i, t0 in enumerate([0, 0.5, 1, 2, 3, 4, 6]):
+    main_df = load_panda("HHG", f"exp_{t0}/exp_laser/{MODEL}", "current_density.json.gz", 
                         **hhg_params(T=300, E_F=118, v_F=v_F, band_width=W, field_amplitude=1., photon_energy=1., decay_time=100))
     times2 = np.linspace(0, main_df["t_end"] - main_df["t_begin"], len(main_df["current_density_time"])) / (2 * np.pi)
     
     t0_unitless = t0 * main_df["photon_energy"] / (2*np.pi * 0.6582119569509065) # the number is hbar in meV * ps
-    axes[0].plot(times2, combined_inter(times2, t0_unitless), label=f"$t_0 = {t0}$ ps")
+    plot_data_combined = combined_inter(times2, t0_unitless)
+    axes[0].plot(times2, plot_data_combined, label=f"$t_0 = {t0}$ ps")
     cdt.add_current_density_to_plot(main_df, axes[1], normalize=False)
     cdt.add_current_density_to_plot(main_df, axes[2], substract=lambda t: combined_inter(t, t0_unitless), label=f"$t_0 = {t0}$ ps", normalize=False)
+    
+    n = len(times2) * 4
+    dt = times2[1] - times2[0]
+    freqs_scipy = rfftfreq(n, dt)
+    
+    fftplot = np.abs(freqs_scipy * rfft(plot_data_combined, n))
+    axes_fft[0].plot(freqs_scipy, 10**(-i) * fftplot , label=f"$t_0 = {t0}$ ps")
+    fftplot = np.abs(freqs_scipy * rfft(main_df["current_density_time"], n))
+    axes_fft[1].plot(freqs_scipy, 10**(-i) * fftplot )
+    fftplot = np.abs(freqs_scipy * rfft(main_df["current_density_time"] - plot_data_combined, n))
+    axes_fft[2].plot(freqs_scipy, 10**(-i) * fftplot )
 
     #ax_laser.plot(times2, combined_laser(times2, t0_unitless), c=f"C{i}")
     #ax_laser.plot(times2, main_df["laser_function"], ls="--", c=f"C{i}", linewidth=4)
@@ -69,6 +87,9 @@ for i, t0 in enumerate([0, 0.5, 1, 2, 3, 4]):
 axes[0].legend(loc='upper right')
 #axes[1].legend(loc='upper right')
 #axes[2].legend(loc='upper right')
+
+axes_fft[0].legend(loc='upper right')
+fig_fft.tight_layout()
 
 fig.tight_layout()
 
