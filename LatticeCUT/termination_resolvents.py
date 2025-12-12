39b9f6036f5db9782a4e3137b4f6d6a2a2f27171
Joshua <joshua.althueser@gmail.com>
2025-12-12 20:40:40 +0100

    multi_termination

--- LatticeCUT/termination_resolvents.py
+++ LatticeCUT/termination_resolvents.py
@@ -7,14 +7,10 @@ from get_data import *
 import continued_fraction_pandas as cf
 
 SYSTEM = "bcc"
-G   = 1.5
+G   = 2.0
 E_F = 0
 OMEGA_D = 0.02
 N = 16000
-
-step = 10
-ignore_firsts = 250 + np.arange(-50, 100, step)
-ignore_lasts  = 250 + np.arange(-50, 100, step) + step
 
 fig, axes = plt.subplots(nrows=2, sharex=True, sharey=True)
 fig.subplots_adjust(hspace=0, wspace=0)
@@ -34,23 +30,36 @@ cmap = plt.get_cmap("viridis")
 avg_spectral_higgs = np.zeros_like(w_lin)
 avg_spectral_phase = np.zeros_like(w_lin)
 
-for i, (ifirst, ilast) in enumerate(zip(ignore_firsts, ignore_lasts)):
-    resolvents = cf.ContinuedFraction(main_df, ignore_first=ifirst, ignore_last=ilast, messages=False)
-    
-    spectral_higgs = resolvents.spectral_density(w_lin, "amplitude_SC", withTerminator=True)
-    spectral_phase = resolvents.spectral_density(w_lin, "phase_SC",     withTerminator=True)
-    
-    axes[0].plot(w_lin.real, spectral_higgs, c=cmap(i/(len(ignore_firsts)-1)), label=f"$[{ifirst},{ilast}]$")
-    axes[1].plot(w_lin.real, spectral_phase, c=cmap(i/(len(ignore_firsts)-1)), label=f"$[{ifirst},{ilast}]$")
-    
-    avg_spectral_higgs += spectral_higgs
-    avg_spectral_phase += spectral_phase
-    
-avg_spectral_higgs /= len(ignore_firsts)
-avg_spectral_phase /= len(ignore_firsts)
+resolvents = cf.ContinuedFraction(main_df, ignore_first=120, ignore_last=250)
+step = 2
+shift_range = np.arange(-20, 20, step)
 
-axes[0].plot(w_lin.real, avg_spectral_higgs, c="k", ls="--", label="avg")
-axes[1].plot(w_lin.real, avg_spectral_phase, c="k", ls="--", label="avg")
+import time
+start = time.time()
+all_higgs = resolvents.spectral_density_varied_depth(w_lin, "amplitude_SC", shift_range)
+all_phase = resolvents.spectral_density_varied_depth(w_lin, "phase_SC", shift_range)
+end = time.time()
+print(end - start)
+
+for i, (higgs, phase) in enumerate(zip(all_higgs, all_phase)):
+    avg_spectral_higgs += higgs
+    avg_spectral_phase += phase
+    
+    axes[0].plot(w_lin.real, higgs, c=cmap(i / (len(shift_range) - 1)))
+    axes[1].plot(w_lin.real, phase, c=cmap(i / (len(shift_range) - 1)))
+
+avg_spectral_higgs /= len(shift_range)
+avg_spectral_phase /= len(shift_range)
+axes[0].plot(w_lin.real, avg_spectral_higgs, c="k", ls="--", label="Average")
+axes[1].plot(w_lin.real, avg_spectral_phase, c="k", ls="--", label="Average")
+
+deviations = np.zeros(len(all_higgs))
+for i, (higgs, phase) in enumerate(zip(all_higgs, all_phase)):
+    deviations[i] = np.linalg.norm(higgs - avg_spectral_higgs) + np.linalg.norm(phase - avg_spectral_phase)
+best_idx = np.argmin(deviations)
+
+axes[0].plot(w_lin.real, all_higgs[best_idx], "r:", linewidth=2, label="Closest to average")
+axes[1].plot(w_lin.real, all_phase[best_idx], "r:", linewidth=2, label="Closest to average")
 
 for ax in axes:
     resolvents.mark_continuum(ax, label=None) 
@@ -58,7 +67,7 @@ for ax in axes:
     ax.set_xlim(np.min(w_lin.real), np.max(w_lin.real))
     ax.set_xlabel(r"$\omega$")
     
-#ax.legend(loc="upper right")
+ax.legend(loc="upper right")
 axes[0].set_ylabel(r"$\mathcal{A}_\mathrm{Higgs} (\omega)$")
 axes[1].set_ylabel(r"$\mathcal{A}_\mathrm{Phase} (\omega)$")
 
