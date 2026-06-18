6fa36151d827a7a93628fd8c0e4f7aeb2a5caa46
Joshua <joshua.althueser@gmail.com>
2026-06-18 15:54:03 +0200

    available data 2

--- LatticeCUT/availabe_data.py
+++ LatticeCUT/availabe_data.py
@@ -5,7 +5,10 @@ from get_data import *
 #df = load_pickle(f"lattice_cut/T_C/bcc", "T_C.pkl").query("N==10000")
 df = load_pickle(f"lattice_cut/bcc/N=16000", "resolvents.pkl")
 
-filtered = df.query("U==0.01 & E_F==-0.5 & omega_D==0.02 & g>2").sort_values('g')
-
+#filtered = df.query("U==0.01 & E_F==-0.5 & omega_D==0.02 & g>2").sort_values('g')
+#for _, row in filtered.iterrows():
+#    print(row["g"])
+    
+filtered = df.query("E_F==-0.5 & omega_D==0.02 & g==2.2").sort_values('g')
 for _, row in filtered.iterrows():
-    print(row["g"])
+    print(row["U"])
