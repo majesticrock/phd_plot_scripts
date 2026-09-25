1386a63c5b8ba7bf4ab333016180e75044d1de0a
Joshua <joshua.althueser@gmail.com>
2026-09-25 21:14:01 +0200

    resovent_U update

--- LatticeCUT/resolvent_vs_U.py
+++ LatticeCUT/resolvent_vs_U.py
@@ -13,28 +13,42 @@ G=1.5
 n_mode = 0
 
 main_df = data_loader.load_pickle(f"lattice_cut", f"{DOS}/N={N}", "resolvents.pkl").query(
-    f"E_F == {E_F} & omega_D == {OMEGA_D} & g == {G} & U>=1").sort_values("U", ignore_index=True)
+    f"E_F == {E_F} & omega_D == {OMEGA_D} & g == {G} & U>0.1").sort_values("U", ignore_index=True)
+Us = np.array([0.2, 0.3, 0.5, 1.0, 2.0, 3.0, 4.0, 5.0, 7.5, 10.0])
+main_df = main_df[main_df["U"].isin(Us)]
 
-color_values = main_df["U"].to_numpy()
-norm = mpl.colors.Normalize(
-    vmin=color_values.min(),
-    vmax=color_values.max()
+norm = mpl.colors.PowerNorm(
+    0.5,
+    vmin=Us.min(),
+    vmax=Us.max()
 )
-cmap = plt.get_cmap("inferno")
-n_lines = len(main_df.index)
+cmap = mpl.colors.LinearSegmentedColormap.from_list(
+    "inferno_truncated",
+    plt.get_cmap("inferno")(np.linspace(0, 0.9, 256))
+)
 
-w_lin = np.linspace(0, 0.5, 2000, dtype=complex)
+w_lin = np.linspace(0, 0.45, 2000, dtype=complex)
 w_lin += 1e-4j
 
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(layout="constrained")
 ax.set_xlabel(r"$\omega / W$")
-ax.set_ylabel(r"$\mathcal{A} (\omega) / W^{-1}$")
+ax.set_ylabel(r"$U^2 \times \mathcal{A}_\mathrm{Higgs} (\omega) / W^{-1}$")
+
+collected_higgs = []
 
 for i, row in main_df.iterrows():
-    resolvents = cf.ContinuedFraction(row, ignore_first=260, ignore_last=280)
-    A_higgs = resolvents.spectral_density(w_lin, "amplitude_SC", with_terminator=True)
-    ax.plot(w_lin.real, row["U"]**2 * A_higgs, c=cmap(i / n_lines))
-    
+    if row["U"] < 0.3:
+        resolvents = cf.ContinuedFraction(row, ignore_first=590, ignore_last=600, messages=False)
+    else:
+        resolvents = cf.ContinuedFraction(row, ignore_first=320, ignore_last=350, messages=False)
+    A_higgs = resolvents.spectral_density(w_lin, "phase_SC", with_terminator=True)
+    collected_higgs.append(A_higgs.tolist())
+    ax.plot(w_lin.real, row["U"]**2 * A_higgs, c=cmap(norm(row["U"])))
+
+data_dict = {"omegas": w_lin.real,
+             "Us" : Us,
+             "A_Higgs" : collected_higgs}
+
 sm = mpl.cm.ScalarMappable(norm=norm, cmap=cmap)
 sm.set_array([])  # compatibility with older matplotlib versions
 cbar = fig.colorbar(sm, ax=ax)
